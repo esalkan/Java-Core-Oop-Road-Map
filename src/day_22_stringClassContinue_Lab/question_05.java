f16c2faa63d6081e9848269ee662d49fca9622fa
esalkan <esalkan@icloud.com>
2021-10-27 14:48:43 +0300

    Lab 3 - Question 5

---
+++ src/day_22_stringClassContinue_Lab/question_05.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package day_22_stringClassContinue_Lab;
+
+/**
+ * @author codesofmine
+ *
+ */
+public class question_05 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * Given a string of even length, return the first half. So the string "WooHoo"
+		 * yields "Woo". 
+		 * 
+		 * firstHalf("WooHoo") → "Woo" 
+		 * firstHalf("HelloThere") → "Hello"
+		 * firstHalf("abcdef") → "abc"
+		 */
+		
+		firstHalf("WooHoo");
+		System.out.println();
+		firstHalf("HelloThere");
+		System.out.println();
+		firstHalf("abcdef");
+		System.out.println();
+		firstHalf("Java & Selenium");
+		System.out.println();
+		firstHalf("Human it is, flows as it is water, curly");
+		System.out.println();
+	}
+
+	private static void firstHalf(String string) {
+		int length = string.length()/2;
+		for(int i = 0; i < length; i++) {
+			System.out.print(string.charAt(i));
+		}
+		
+	}
+
+}
