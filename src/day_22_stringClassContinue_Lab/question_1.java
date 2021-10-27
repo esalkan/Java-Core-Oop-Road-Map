381d520fb6b053217caa03ba4cbdd3e3a7433644
esalkan <esalkan@icloud.com>
2021-10-27 12:26:06 +0300

    Lab-3 Question 1

---
+++ src/day_22_stringClassContinue_Lab/question_1.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package day_22_stringClassContinue_Lab;
+
+/**
+ * @author codesofmine
+ *
+ */
+public class question_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * Create a method that accepts 1 string, e.g. "Bob", return a greeting of the
+		 * form "Hello Bob!". 
+		 * 
+		 * helloName("Bob") → "Hello Bob!" 
+		 * 
+		 * helloName("Alice") → "Hello Alice!" 
+		 * 
+		 * helloName("X") → "Hello X!"
+		 */
+
+	}
+
+}
