64f3bede255d87adc5b448aa2bc72257baae26df
esalkan <esalkan@icloud.com>
2021-10-27 13:06:09 +0300

    Lab 3 - Question 2

---
+++ src/day_22_stringClassContinue_Lab/question_2.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package day_22_stringClassContinue_Lab;
+
+/**
+ * @author codesofmine
+ *
+ */
+public class question_2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * Create a method that accepts two strings, a and b, return the result of
+		 * putting them together in the order abba, e.g. "Hi" and "Bye" returns
+		 * "HiByeByeHi".
+		 * 
+		 * makeAbba("Hi", "Bye") → "HiByeByeHi" 
+		 * makeAbba("Yo", "Alice") → "YoAliceAliceYo" 
+		 * makeAbba("What", "Up") → "WhatUpUpWhat"
+		 */
+
+		makeAbba("Hi", "Bye");
+		makeAbba("Yo", "Alice");
+		makeAbba("What", "Up");
+	}
+
+	private static void makeAbba(String string, String string2) {
+		String combine = string.concat(string2);
+		String replacing = combine.replace(combine,string2.concat(string));
+		System.out.println(combine+replacing);
+	}
+
+}
