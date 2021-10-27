1383a187384512b0737e48eeb1b8c0e030b239d2
esalkan <esalkan@icloud.com>
2021-10-27 14:54:57 +0300

    Lab 3 - Question 6

--- src/day_22_stringClassContinue_Lab/question_06.java
+++ src/day_22_stringClassContinue_Lab/question_06.java
@@ -13,8 +13,25 @@ public class question_06 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/**
+		 * Given a string, return a version without the first and last char, so "Hello"
+		 * yields "ell". The string length will be at least 2. 
+		 * 
+		 * withoutEnd("Hello") → "ell" 
+		 * withoutEnd("java") → "av" 
+		 * withoutEnd("coding") → "odin"
+		 */
 
+		withoutEnd("Hello");
+		withoutEnd("java");
+		withoutEnd("coding");
+		withoutEnd("python");
+		withoutEnd("justTest");
+	}
+
+	private static void withoutEnd(String string) {
+		String result = string.substring(1, string.length()-1);
+		System.out.println(result);
 	}
 
 }
