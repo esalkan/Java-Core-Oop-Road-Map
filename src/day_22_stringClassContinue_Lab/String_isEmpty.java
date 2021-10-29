316dff511f92976b3500fb4f62a2fd8d9acdde0c
esalkan <esalkan@icloud.com>
2021-10-29 15:19:39 +0300

    String Class - isEmpty()

---
+++ src/day_22_stringClassContinue_Lab/String_isEmpty.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package day_22_stringClassContinue_Lab;
+
+/**
+ * @author esalkan
+ *
+ */
+public class String_isEmpty {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String userName = "";
+		String password = "";
+		
+		if(userName.isEmpty()) {
+			System.out.println("Username field can not be empty");
+		} else {
+			System.out.println("Username is not empty");
+		}
+		
+		System.out.println("\n+--------------------------------+\n");
+		
+		
+		if (userName.isEmpty() || password.isEmpty()) {
+			System.out.println("Username or password can not be empty");
+		} else {
+			System.out.println("Username or password not empty");
+		}
+	}
+
+}
