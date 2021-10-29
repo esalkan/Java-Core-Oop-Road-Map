03f740bd3f3d8c4b34cc1239ac66ea3ea22c5097
esalkan <esalkan@icloud.com>
2021-10-29 15:15:47 +0300

    Minor Changes

--- src/day_22_stringClassContinue_Lab/question_01.java
+++ src/day_22_stringClassContinue_Lab/question_01.java
@@ -15,23 +15,29 @@ public class question_01 {
 	public static void main(String[] args) {
 		/**
 		 * Create a method that accepts 1 string, e.g. "Bob", return a greeting of the
-		 * form "Hello Bob!". 
+		 * form "Hello Bob!".
 		 * 
-		 * helloName("Bob") → "Hello Bob!" 
+		 * helloName("Bob") → "Hello Bob!"
 		 * 
-		 * helloName("Alice") → "Hello Alice!" 
+		 * helloName("Alice") → "Hello Alice!"
 		 * 
 		 * helloName("X") → "Hello X!"
 		 */
 
-		helloName("Bob");
-		helloName("Alice");
-		helloName("X");
+		System.out.println(helloName("Bob"));
+		System.out.println(helloName("Alice"));
+		System.out.println(helloName("X"));
+		
 	}
-	
-	public static void helloName(String name) {
+
+	public static String helloName(String name) {
 		String greeting = "Hello ";
-		System.out.println(greeting.concat(name).concat("!")); 
+		String result = greeting.concat(name).concat("!");
+		return result;
 	}
+
+//	public static String helloName(String str) {
+//		return "Hello ".concat(str).concat("!");
+//	}
 
 }
