ce2b7bdfa91a422d7d0eb33bb4c568bc72449184
esalkan <esalkan@icloud.com>
2021-10-31 17:42:52 +0300

    not solved question

---
+++ src/day_23_stringClass_lab_randomClass/question_13.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package day_23_stringClass_lab_randomClass;
+
+/**
+ * @author esalkan
+ *
+ */
+public class question_13 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * Given a string, return a string where for every char in the original, there
+		 * are two chars. 
+		 * 
+		 * doubleChar("The") → "TThhee" 
+		 * 
+		 * doubleChar("AAbb") → "AAAAbbbb"
+		 * doubleChar("Hi-There") → "HHii--TThheerree"
+		 */
+		
+
+	}
+}
