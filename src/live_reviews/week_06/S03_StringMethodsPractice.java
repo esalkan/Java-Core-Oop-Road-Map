70db1a1670f416c161e67a60029931d9b20d7c47
esalkan <esalkan@icloud.com>
2021-10-31 17:45:45 +0300

    S03_StringMethodsPractice

---
+++ src/live_reviews/week_06/S03_StringMethodsPractice.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package live_reviews.week_06;
+
+/**
+ * @author esalkan
+ *
+ */
+public class S03_StringMethodsPractice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String message = "Java is Awesome!";
+		
+		// length() : takes String as parameter and returns int
+		int count = message.length();
+		System.out.println("How many characters I have here : " + count);
+		
+		int sum = 4 + message.length();
+		System.out.println("sum : " + sum);
+		
+		// for iterating through our string object
+		
+	}
+
+}
