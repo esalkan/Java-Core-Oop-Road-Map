c081d0a07060d5b87da581f5bc651d0c25deac28
esalkan <esalkan@icloud.com>
2021-11-01 23:15:49 +0300

    Create task_85_DiceGame.java
    not solved

---
+++ src/day_23_stringClass_lab_randomClass/task_85_DiceGame.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package day_23_stringClass_lab_randomClass;
+
+/**
+ * @author esalkan
+ *
+ */
+public class task_85_DiceGame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * Write a program that plays a simple dice game between the computer and the
+		 * user. When the program runs, a loop should repeat 10 times. Each iteration of
+		 * the loop should do the following:
+		 * 
+		 * -> Generate a random integer in the range of 1 thru 6. This is the value of
+		 * the computer’s dice.
+		 * 
+		 * -> Generate another random integer in the range of 1 thru 6. This is the
+		 * value of the user’s dice.
+		 * 
+		 * -> The dice with the highest value wins. (In case of a tie, there is no
+		 * winner)
+		 * 
+		 * As the loop iterates , the program should keep count of the number of times
+		 * the computer wins, and the number of times that the user wins. After the loop
+		 * performs all of its iterations, the program should display who was the grand
+		 * winner, the computer or the user.
+		 */
+
+
+		
+		String n ="012345678";
+		System.out.println(n.substring(7));
+		
+		
+		String s ="purr";
+		s.substring(1,3);
+		s += " two";
+		System.out.println(s.length());
+
+	}
+
+}
