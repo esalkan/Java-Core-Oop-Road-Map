3ff967bd214095451b405f112eae6c9810fc7c68
esalkan <esalkan@icloud.com>
2021-11-01 21:34:00 +0300

    Create randomClass.java

---
+++ src/day_23_stringClass_lab_randomClass/randomClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package day_23_stringClass_lab_randomClass;
+
+/**
+ * @author esalkan
+ *
+ */
+public class randomClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
