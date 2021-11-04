b16edeb21c8325432c04872a7d681ea0e7b68a57
esalkan <esalkan@icloud.com>
2021-11-04 20:10:06 +0300

    Create isPalindrome.java

---
+++ src/liveMeetings/isPalindrome.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package liveMeetings;
+
+/**
+ * @author esalkan
+ *
+ */
+public class isPalindrome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String str = "radar";
+		
+		String revRes = "";
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+			revRes += str.charAt(i);
+		}
+
+		if (revRes.equalsIgnoreCase(str)) {
+			System.out.println("Yes word is palindrome");
+		} else {
+			System.out.println("No word is not palindrome");
+		}
+	}
+
+}
