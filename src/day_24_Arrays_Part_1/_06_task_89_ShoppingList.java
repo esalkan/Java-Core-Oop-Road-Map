e373a01125e29eb40c92fee9b5757938fc2af5b9
esalkan <esalkan@icloud.com>
2021-11-10 11:30:58 +0300

    Create _06_task_89_ShoppingList.java

---
+++ src/day_24_Arrays_Part_1/_06_task_89_ShoppingList.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package day_24_Arrays_Part_1;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _06_task_89_ShoppingList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[] shoppingList = { "cheese", "pumpkin", "bread", "eggs", "milk" };
+
+		for (int i = 0; i < shoppingList.length; i++) {
+
+			System.out.println(shoppingList[i]);
+		}
+	}
+}
