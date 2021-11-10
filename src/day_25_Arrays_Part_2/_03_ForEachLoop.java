b1684ace379c8ec85ef418e6195709d12a0519fc
esalkan <esalkan@icloud.com>
2021-11-10 13:55:04 +0300

    Create _03_ForEachLoop.java

---
+++ src/day_25_Arrays_Part_2/_03_ForEachLoop.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package day_25_Arrays_Part_2;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _03_ForEachLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * the for-each loop is used to access each successive value in a collection of
+		 * values. The main usage of for-each is iterating over collection classes or
+		 * arrays.
+		 */
+
+		/**
+		 * Syntax
+		 * 
+		 * for (dataType elementVariable : array) { 
+		 * 	// Statement 
+		 * }
+		 * 
+		 */
+		
+		int[] numbers = {3,6,9};
+		
+		for (int i : numbers) {
+			System.out.println(i);
+		}
+	}
+
+}
