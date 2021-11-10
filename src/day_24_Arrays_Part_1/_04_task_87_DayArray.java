032fb067a18d72a4c55805241ebc418b1544a558
esalkan <esalkan@icloud.com>
2021-11-10 11:31:04 +0300

    Create _04_task_87_DayArray.java

---
+++ src/day_24_Arrays_Part_1/_04_task_87_DayArray.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package day_24_Arrays_Part_1;
+
+import java.util.Scanner;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _04_task_87_DayArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String[] days = new String[7];
+		
+		days[0] = "Monday";
+		days[1] = "Tuesday";
+		days[2] = "Wednesday";
+		days[3] = "Thursday";
+		days[4] = "Friday";
+		days[5] = "Saturday";
+		days[6] = "Sunday";
+		
+		System.out.println("Enter your day index : ");
+		int index = sc.nextInt();
+		
+		System.out.println("The day is : " + days[index]);
+	}
+
+}
