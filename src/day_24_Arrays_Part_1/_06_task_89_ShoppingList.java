f19bf13d3158f6fb9fa43e8e3bc58ad374f38020
esalkan <esalkan@icloud.com>
2021-11-10 11:35:56 +0300

    Update _06_task_89_ShoppingList.java

--- src/day_24_Arrays_Part_1/_06_task_89_ShoppingList.java
+++ src/day_24_Arrays_Part_1/_06_task_89_ShoppingList.java
@@ -13,6 +13,9 @@ public class _06_task_89_ShoppingList {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		/**
+		 * Display each array element in the console by using for loop
+		 */
 		String[] shoppingList = { "cheese", "pumpkin", "bread", "eggs", "milk" };
 
 		for (int i = 0; i < shoppingList.length; i++) {
