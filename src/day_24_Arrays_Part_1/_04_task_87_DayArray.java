fa34eebe14a739b6240e2a1416ba3014aadb92cf
esalkan <esalkan@icloud.com>
2021-11-10 11:35:52 +0300

    Update _04_task_87_DayArray.java

--- src/day_24_Arrays_Part_1/_04_task_87_DayArray.java
+++ src/day_24_Arrays_Part_1/_04_task_87_DayArray.java
@@ -15,10 +15,16 @@ public class _04_task_87_DayArray {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		/**
+		 * Create an array that holds the days User should be able to enter the day
+		 * index and output should be:
+		 * 
+		 * “Today is Monday”
+		 */
 		Scanner sc = new Scanner(System.in);
-		
+
 		String[] days = new String[7];
-		
+
 		days[0] = "Monday";
 		days[1] = "Tuesday";
 		days[2] = "Wednesday";
@@ -26,10 +32,10 @@ public class _04_task_87_DayArray {
 		days[4] = "Friday";
 		days[5] = "Saturday";
 		days[6] = "Sunday";
-		
+
 		System.out.println("Enter your day index : ");
 		int index = sc.nextInt();
-		
+
 		System.out.println("The day is : " + days[index]);
 	}
 
