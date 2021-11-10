af26e9c4f8c7ef7f19d75a8c3d42edaf643656c1
esalkan <esalkan@icloud.com>
2021-11-10 22:14:31 +0300

    Create _06_PassElements.java

---
+++ src/day_25_Arrays_Part_2/_06_PassElements.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package day_25_Arrays_Part_2;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _06_PassElements {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int[] numbers = { 541, 0, 1, 2, 3, 4, 5, 1453 };
+
+		for (int value : numbers) {
+			showValue(value);
+		}
+
+	}
+
+	public static void showValue(int n) {
+		System.out.print(n + " ");
+	}
+
+}
