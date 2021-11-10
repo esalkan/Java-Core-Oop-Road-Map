72066cddc94257e7cb32fa1e4dc9999a37c4f488
esalkan <esalkan@icloud.com>
2021-11-10 11:35:58 +0300

    Update _07_task_90_FindingCar.java

--- src/day_24_Arrays_Part_1/_07_task_90_FindingCar.java
+++ src/day_24_Arrays_Part_1/_07_task_90_FindingCar.java
@@ -13,7 +13,15 @@ public class _07_task_90_FindingCar {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		/**
+		 * Create a car array that holds 6 car names inside
+		 * 
+		 * Check if your array has the car name “Honda”
+		 * 
+		 * If it is , print “ I found your car Honda in this array”
+		 * 
+		 * If it is not, print “ I could not find your car Honda in this array”
+		 */
 		String[] cars = { "Toyota", "Nissan", "BMW", "Mercedes", "Honda" };
 
 		boolean flag = false;
