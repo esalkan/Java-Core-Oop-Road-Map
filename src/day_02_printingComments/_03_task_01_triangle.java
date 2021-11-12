0ef352232ac308b56cdec05bb55778376d2f361d
esalkan <esalkan@icloud.com>
2021-11-12 17:58:11 +0300

    Update _03_task_01_triangle.java

--- src/day_02_printingComments/_03_task_01_triangle.java
+++ src/day_02_printingComments/_03_task_01_triangle.java
@@ -13,7 +13,7 @@ public class _03_task_01_triangle {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// print a rectangle with print method
 		System.out.println("    *");
 		System.out.println("   * * ");
 		System.out.println("  *   *");
