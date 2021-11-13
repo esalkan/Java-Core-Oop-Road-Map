5e300d6a077bd677a581825919964b955aad6283
esalkan <esalkan@icloud.com>
2021-11-13 13:01:46 +0300

    Create Question_09.java

---
+++ src/day_27_Arrays_Part_4/Lab_4/Question_09.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package day_27_Arrays_Part_4.Lab_4;
+
+import java.util.Arrays;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Question_09 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * 
+		 * Question-9
+		 * 
+		 * Write a method that accepts an array and prints a new array with double the
+		 * length where its last element is the same as the original array,
+		 * 
+		 * int[] x = {4,5,6}; = > [0,0,0,0,0,6]
+		 * 
+		 */
+
+		int[] x = { 4, 5, 6, 8 };
+		System.out.println(Arrays.toString(doubleArray(x)));
+
+	}
+
+	public static int[] doubleArray(int[] arr) {
+		int[] result = new int[arr.length * 2];
+		int swap = 0;
+
+		for (int i = 0; i < result.length; i++) {
+			if (i < result.length) {
+				result[i] = 0;
+			}
+
+			if (i == result.length - 1) {
+				result[result.length - 1] = arr[arr.length - 1];
+			}
+		}
+		return result;
+
+	}
+
+}
