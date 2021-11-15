35965a991a7d1c92a6088ad147afe3bf3a7fa162
esalkan <esalkan@icloud.com>
2021-11-15 19:33:02 +0300

    Create _01_Arrays.java

---
+++ src/day_28_Arrays_Part_5/_01_Arrays.java
@@ -0,0 +1,17 @@
+package day_28_Arrays_Part_5;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _01_Arrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
