b7ec4bc65abc6a59754ba380a13cbef7810a548e
esalkan <esalkan@icloud.com>
2021-11-18 09:42:24 +0300

    Create Arrays_Part_6.java

---
+++ src/day_29_Arrays_Part_6/Arrays_Part_6.java
@@ -0,0 +1,17 @@
+package day_29_Arrays_Part_6;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Arrays_Part_6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
