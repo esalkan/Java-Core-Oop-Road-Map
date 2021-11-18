b86e003a4d93fc2fb380ee8b8fd8780c5589438f
esalkan <esalkan@icloud.com>
2021-11-18 12:35:50 +0300

    Create Question_26.java

---
+++ src/day_29_Arrays_Part_6/Lab_4_Cont/Question_26.java
@@ -0,0 +1,54 @@
+package day_29_Arrays_Part_6.Lab_4_Cont;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Question_26 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * Write a method that accepts an array and prints true if the array contains 3
+		 * even or 3 odd values all next to each other.
+		 * 
+		 * int x[] = {2,1,3,5}; -> true
+		 * 
+		 * int y[] = {2,1,2,5}; -> false
+		 * 
+		 * int z[] = {2,4,2,5}; -> true
+		 */
+
+		int x[] = { 2, 1, 3, 5 };
+		int y[] = { 2, 1, 2, 5 };
+		int z[] = { 2, 4, 2, 5 };
+		
+		System.out.println(isContainEvenOrOdd(x));
+		System.out.println(isContainEvenOrOdd(y));
+		System.out.println(isContainEvenOrOdd(z));
+
+	}
+
+	public static boolean isContainEvenOrOdd(int[] arr) {
+
+		int countEven = 0;
+		int countOdd = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] % 2 == 0) {
+				countEven++;
+			} else {
+				countOdd++;
+			}
+		}
+
+		if (countEven == 3 || countOdd == 3) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
