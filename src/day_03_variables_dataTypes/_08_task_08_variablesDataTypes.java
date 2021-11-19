604c3010f70fd503b2349b2d7b2958b1cddd6f9a
esalkan <esalkan@icloud.com>
2021-11-19 17:09:32 +0300

    Update _08_task_08_variablesDataTypes.java

--- src/day_03_variables_dataTypes/_08_task_08_variablesDataTypes.java
+++ src/day_03_variables_dataTypes/_08_task_08_variablesDataTypes.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package day_03_variables_dataTypes;
 
 /**
@@ -19,7 +16,6 @@ public class _08_task_08_variablesDataTypes {
 	// above from 1491 to 1492.
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int date = 1492;
 		String text = "IN " + date + " COLUMBUS SAILED THE OCEANBLUE";
 		
