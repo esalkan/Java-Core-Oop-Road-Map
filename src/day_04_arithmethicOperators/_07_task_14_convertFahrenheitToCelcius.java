d670b5c0362cb875a5eff4dcc59159b9646d45bc
esalkan <esalkan@icloud.com>
2021-11-19 17:09:14 +0300

    Update _07_task_14_convertFahrenheitToCelcius.java

--- src/day_04_arithmethicOperators/_07_task_14_convertFahrenheitToCelcius.java
+++ src/day_04_arithmethicOperators/_07_task_14_convertFahrenheitToCelcius.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package day_04_arithmethicOperators;
 
 /**
@@ -13,7 +10,6 @@ public class _07_task_14_convertFahrenheitToCelcius {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		double fahrenheit, celcius;
 		celcius = 70.2;
 		fahrenheit = 9 * celcius / 5 + 32;
