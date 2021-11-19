fc330138014f642bdcb5f952e7f7cc0c25a5717a
esalkan <esalkan@icloud.com>
2021-11-19 17:05:33 +0300

    Update _06_escapeSequences.java

--- src/day_02_printingComments/_06_escapeSequences.java
+++ src/day_02_printingComments/_06_escapeSequences.java
@@ -1,10 +1,7 @@
-/**
- * 
- */
 package day_02_printingComments;
 
 /**
- * @author codesofmine
+ * @author esalkan
  *
  */
 public class _06_escapeSequences {
@@ -13,14 +10,21 @@ public class _06_escapeSequences {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*
+		/**
 		 * \n = New Line -> Advances the cursor to the next line for subsequent
-		 * printing. \t = Horizontal Tab -> Causes the cursor to skip over to the next
-		 * tab stop. \b = Backspace -> Causes the cursor to back up, or move left, one
-		 * position. \r = Return -> Causes the cursor to go to the beginning of the
-		 * current line, not the next line. \\ = Backslash -> Causes a backslash to be
-		 * printed. \' = Single quote -> Causes a single quotation mark to be printed.
+		 * printing.
+		 * 
+		 * \t = Horizontal Tab -> Causes the cursor to skip over to the next tab stop.
+		 * 
+		 * \b = Backspace -> Causes the cursor to back up, or move left, one position.
+		 * 
+		 * \r = Return -> Causes the cursor to go to the beginning of the current line,
+		 * not the next line.
+		 * 
+		 * \\ = Backslash -> Causes a backslash to be printed.
+		 * 
+		 * \' = Single quote -> Causes a single quotation mark to be printed.
+		 * 
 		 * \" = Double quote -> Causes a double quotation mark to be printed.
 		 *
 		 */
