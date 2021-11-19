ad3854617f6521e3f9810dad8eb7bb5a6fa80a2a
esalkan <esalkan@icloud.com>
2021-11-19 17:09:44 +0300

    Update _01_variableDeclaration.java

--- src/day_03_variables_dataTypes/_01_variableDeclaration.java
+++ src/day_03_variables_dataTypes/_01_variableDeclaration.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package day_03_variables_dataTypes;
 
 /**
@@ -13,7 +10,6 @@ public class _01_variableDeclaration {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		// Declaration
 		byte inches;
 		int speed;
