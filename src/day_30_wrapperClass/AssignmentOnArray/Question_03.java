70a795ab25c883d3287cc77e2c18d06d0ceaef4e
esalkan <esalkan@icloud.com>
2021-11-21 14:48:15 +0300

    Update Question_03.java

--- src/day_30_wrapperClass/AssignmentOnArray/Question_03.java
+++ src/day_30_wrapperClass/AssignmentOnArray/Question_03.java
@@ -10,7 +10,28 @@ public class Question_03 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/**
+		 * Below is One-dimensional array code:
+		 * 
+		 * char arr [] = new char[] {'A', 'Z', 'B', 'N', 'P', 'T', 'X', 'C'};
+		 * 
+		 * Write the code to search for particular character in above array. If I search
+		 * for 'B', then output should be: "B found!" If I search for 'E', then output
+		 * should be: "E Not found!"
+		 */
+
+		char arr[] = new char[] { 'A', 'Z', 'B', 'N', 'P', 'T', 'X', 'C' };
+		Character search = 'Y';
+
+		for(Character value : arr) {
+			if (value.equals(search)) {
+				System.out.println(value + " Found!");
+				break;
+			} else {
+				System.out.println(search + " Not Found!.");
+				break;
+			}
+		}
 
 	}
 
