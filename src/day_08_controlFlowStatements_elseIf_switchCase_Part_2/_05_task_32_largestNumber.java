df9038cdaec241580c756ec7b653556911bb82c3
esalkan <esalkan@icloud.com>
2021-11-23 09:23:47 +0300

    Update _05_task_32_largestNumber.java

--- src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_05_task_32_largestNumber.java
+++ src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_05_task_32_largestNumber.java
@@ -16,29 +16,28 @@ public class _05_task_32_largestNumber {
 
 		// Declare 3 Numbers
 		// Program should display which one is largest
-		
-		int num1=10000;
-		int num2=2000;
-		int num3=300;
-		
-		if((num1>num2)&&(num1>num3)) {
-			
+
+		int num1 = 10000;
+		int num2 = 2000;
+		int num3 = 300;
+
+		if ((num1 > num2) && (num1 > num3)) {
+
 			System.out.println("Num1 is greatest one");
-			
-		}else if((num2>num1)&&(num2>num3)) {
-			
+
+		} else if ((num2 > num1) && (num2 > num3)) {
+
 			System.out.println("Num2 is greatest one");
-			
-		}else if((num3>num1)&&(num3>num2)) {
-			
+
+		} else if ((num3 > num1) && (num3 > num2)) {
+
 			System.out.println("Num3 is greatest one");
-			
-		}else {
-			
+
+		} else {
+
 			System.out.println("Numbers are equal");
 		}
-		
-		
+
 	}
 
 }
