353f75991e72a29610fface3c8648205d04ad060
esalkan <esalkan@icloud.com>
2021-11-23 09:24:06 +0300

    Update _06_ifElseRules.java

--- src/day_09_controlFlowStatements_Part_3/_06_ifElseRules.java
+++ src/day_09_controlFlowStatements_Part_3/_06_ifElseRules.java
@@ -13,7 +13,6 @@ public class _06_ifElseRules {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		if (true)
 			System.out.println("Hello");
