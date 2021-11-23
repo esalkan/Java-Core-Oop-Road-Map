81a663274fe9ff6a34678d44537c9153f1d14a9f
esalkan <esalkan@icloud.com>
2021-11-23 09:24:01 +0300

    Update _12_task_36_switchCase_Calculator.java

--- src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_12_task_36_switchCase_Calculator.java
+++ src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_12_task_36_switchCase_Calculator.java
@@ -13,32 +13,35 @@ public class _12_task_36_switchCase_Calculator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		int num1=10;
-		int num2=5;
-		String operator="/";
-		int result=0;
-		
-		switch(operator) {
-		
+		int num1 = 10;
+		int num2 = 5;
+		String operator = "/";
+		int result = 0;
+
+		switch (operator) {
+
 		case "+":
-			result = num1+num2;
+			result = num1 + num2;
 			break;
+
 		case "-":
-			result = num1-num2;
+			result = num1 - num2;
 			break;
+
 		case "*":
-			result = num1*num2;
+			result = num1 * num2;
 			break;
+
 		case "/":
-			result = num1/num2;
+			result = num1 / num2;
 			break;
+
 		default:
 			System.out.println("Invalid Operator");
-		
+
 		}
-		
+
 		System.out.println("Result is " + result);
 	}
 
