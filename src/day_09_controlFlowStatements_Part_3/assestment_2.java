09c2631f0175d35ab2579f8939b723f354870848
esalkan <esalkan@icloud.com>
2021-11-23 09:24:07 +0300

    Update assestment_2.java

--- src/day_09_controlFlowStatements_Part_3/assestment_2.java
+++ src/day_09_controlFlowStatements_Part_3/assestment_2.java
@@ -13,11 +13,10 @@ public class assestment_2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		boolean x = true;
-		boolean y = !x==false;
-	
+		boolean y = !x == false;
+
 		System.out.println(y);
 	}
 
