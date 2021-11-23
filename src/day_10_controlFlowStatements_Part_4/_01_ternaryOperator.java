45bbb139678aca8a0446e51632ca08b94c12ab8c
esalkan <esalkan@icloud.com>
2021-11-23 09:24:09 +0300

    Update _01_ternaryOperator.java

--- src/day_10_controlFlowStatements_Part_4/_01_ternaryOperator.java
+++ src/day_10_controlFlowStatements_Part_4/_01_ternaryOperator.java
@@ -13,11 +13,10 @@ public class _01_ternaryOperator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		//	String result;
+		String result;
 				
-		//	int score = 88;
+		int score = 88;
 		//	
 		//	if (score > 60) {
 		//		result = "Pass...";
@@ -27,26 +26,26 @@ public class _01_ternaryOperator {
 				
 				
 				
-		//	                    ?    and     :	are ternary operators
-		//	result = score > 60 ?  "Pass..." : "Fail...";
+		//	                ?    and     :	are ternary operators
+		result = score > 60 ?  "Pass..." : "Fail...";
 				
-		//	System.out.println(result);
+		System.out.println(result);
 		
 				
 		//	Nested If condition 		
-		//	int x = 5;
+			int x = 5;
 				
-		//	System.out.println(x > 2 ? x < 4 ? 10 : 8: 7);
-		//	
-		//	if (x > 2) {
-		//		if(x < 4) {
-		//			System.out.println(10);
-		//		} else {
-		//			System.out.println(8);
-		//		}
-		//	} else {
-		//		System.out.println(7);
-		//	}
+			System.out.println(x > 2 ? x < 4 ? 10 : 8 : 7);
+			
+			if (x > 2) {
+				if(x < 4) {
+					System.out.println(10);
+				} else {
+					System.out.println(8);
+				}
+			} else {
+				System.out.println(7);
+			}
 	}
 
 }
