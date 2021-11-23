725251ab02c4a8a0134e81dc1425c036c9ca3712
esalkan <esalkan@icloud.com>
2021-11-23 09:19:47 +0300

    Update _10_task_27_ifStatement2.java

--- src/day_07_controlFlowStatements_if_ifElse_Part_1/_10_task_27_ifStatement2.java
+++ src/day_07_controlFlowStatements_if_ifElse_Part_1/_10_task_27_ifStatement2.java
@@ -13,7 +13,6 @@ public class _10_task_27_ifStatement2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		// Write a Java Program to implement following logic using if statement
 		// 1- If hour is less then 12 noon, greet with "Good Morning"
@@ -21,7 +20,7 @@ public class _10_task_27_ifStatement2 {
 		//	  Greet with "Good Afternoon"
 		// 3- If hour is greater  than or equal 3pm, greet with "Good Evening"
 		
-		int time = 16;
+		int time = 10;
 		
 		if (time < 12) {
 			System.out.println("Good Morning!");
