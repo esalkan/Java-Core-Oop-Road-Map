899286b8fdbfc05356c190d52a1d479e66f90a50
esalkan <esalkan@icloud.com>
2021-11-23 09:23:49 +0300

    Update _06_task_33_grading.java

--- src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_06_task_33_grading.java
+++ src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_06_task_33_grading.java
@@ -15,22 +15,22 @@ public class _06_task_33_grading {
 	public static void main(String[] args) {
 
 		// Write a Java Program for the following logic:
-		//	§ If marks < 60, then print “Fail”
-		//	§ If marks >= 60, but less than 90, then print “Pass”
-		//	§ If marks >= 90, then print “Passed with Distinction”
-			
-		int mark=90;
-		
-		if(mark<60) {
+		// § If marks < 60, then print “Fail”
+		// § If marks >= 60, but less than 90, then print “Pass”
+		// § If marks >= 90, then print “Passed with Distinction”
+
+		int mark = 90;
+
+		if (mark < 60) {
 			System.out.println("Fail");
-		}else if(mark>=60 && mark<90) {
+		} else if (mark >= 60 && mark < 90) {
 			System.out.println("Pass");
-		}else if(mark>90) {
+		} else if (mark > 90) {
 			System.out.println("Passed with distinction");
-		}else {
+		} else {
 			System.out.println("Invalid Score");
 		}
-		
+
 	}
 
 }
