99ae303eb54691249ca36d67630e65c0ed031ca6
esalkan <esalkan@icloud.com>
2021-11-23 09:23:57 +0300

    Update _09_task_35_findGreatestNested.java

--- src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_09_task_35_findGreatestNested.java
+++ src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_09_task_35_findGreatestNested.java
@@ -17,21 +17,21 @@ public class _09_task_35_findGreatestNested {
 		int n1 = 1000;
 		int n2 = 10000;
 		int n3 = 100111;
-		
+
 		if (n1 > n2 && n1 > n3) {
-			
+
 			System.out.println("The greatest number is => n1 => " + n1);
-			
+
 		} else {
-			
+
 			if (n2 > n1 && n2 > n3) {
 				System.out.println("The greatest number is => n2 => " + n2);
 			} else {
 				System.out.println("The greatest number is => n3 => " + n3);
 			}
-			
+
 		}
-		
+
 	}
 
 }
