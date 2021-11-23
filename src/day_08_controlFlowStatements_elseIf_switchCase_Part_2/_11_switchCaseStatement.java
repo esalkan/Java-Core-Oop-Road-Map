e8dbcaa8d2b24584f933d3d3a1580b8a56e390dc
esalkan <esalkan@icloud.com>
2021-11-23 09:23:59 +0300

    Update _11_switchCaseStatement.java

--- src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_11_switchCaseStatement.java
+++ src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_11_switchCaseStatement.java
@@ -13,32 +13,37 @@ public class _11_switchCaseStatement {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		String weather="snow";
-		switch(weather) {
-		
+		String weather = "snow";
+
+		switch (weather) {
+
 		case "sunny":
 			System.out.println("Go to park");
 			System.out.println("Code Java");
 			break;
+
 		case "hot":
 			System.out.println("Go to swimming");
 			System.out.println("Code Java");
 			break;
-		default:
-			System.out.println("Code Java in any other weather");
-			break;
+
 		case "windy":
+
 			System.out.println("Fly a kite");
 			System.out.println("Code Java");
 			break;
+
 		case "snow":
 			System.out.println("Go snowboarding");
 			System.out.println("Code Java");
 			break;
+
+		default:
+			System.out.println("Code Java in any other weather");
+			break;
 		}
-		
+
 	}
 
 }
