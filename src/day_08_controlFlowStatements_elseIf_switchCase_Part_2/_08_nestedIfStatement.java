ac5f3625a4bc84e8d402ad9093011b03586ad8f4
esalkan <esalkan@icloud.com>
2021-11-23 09:23:55 +0300

    Update _08_nestedIfStatement.java

--- src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_08_nestedIfStatement.java
+++ src/day_08_controlFlowStatements_elseIf_switchCase_Part_2/_08_nestedIfStatement.java
@@ -14,27 +14,37 @@ public class _08_nestedIfStatement {
 	 */
 	public static void main(String[] args) {
 
-		boolean isRushHour=false;
-		int carType=2;
-		double price=0.0;
-		
-		if(carType==1) {
-			if(isRushHour) {
-				price=30.0;
-			}else {
-				price=5.0;
+		boolean isRushHour = true;
+		int carType = 2;
+		double price = 0.0;
+
+		if (carType == 1) {
+
+			if (isRushHour) {
+
+				price = 30.0;
+
+			} else {
+
+				price = 5.0;
+
 			}
-		}else if(carType==2) {
-			if(isRushHour) {
-				price=55.30;
-			}else {
-				price=15.99;
+
+		} else if (carType == 2) {
+
+			if (!isRushHour) {
+
+				price = 55.30;
+
+			} else {
+
+				price = 15.99;
 			}
-			
+
 		}
-		
+
 		System.out.println("Toll Cost: " + price);
-		
+
 	}
 
 }
