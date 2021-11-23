0f2a02f91e49d94a3db7143845e1b1d598f86e58
esalkan <esalkan@icloud.com>
2021-11-23 09:19:38 +0300

    Update _05_smallTasks_task_4_ifStatement2.java

--- src/day_07_controlFlowStatements_if_ifElse_Part_1/_05_smallTasks_task_4_ifStatement2.java
+++ src/day_07_controlFlowStatements_if_ifElse_Part_1/_05_smallTasks_task_4_ifStatement2.java
@@ -16,12 +16,11 @@ public class _05_smallTasks_task_4_ifStatement2 {
 	// Write an if statement that assigns 20 to the variable a if variable b is 50 and c is greater and equal to 100
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int a = 100;
 		int b = 50;
 		int c = 300;
-		
+
 		if (b==50 || c>100) {
 			a =  20;
 		}
