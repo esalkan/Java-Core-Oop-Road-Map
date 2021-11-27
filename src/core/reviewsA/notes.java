f1f2178a464a9da3d5c90e154e09bbdcdf2e2778
esalkan <esalkan@icloud.com>
2021-11-27 19:18:05 +0300

    Create notes.java

---
+++ src/core/reviewsA/notes.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package core.reviewsA;
+
+/**
+ * @author esalkan
+ *
+ */
+public class notes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
