0f149c01d731a75602ded754eaf2783c4b36d8cf
esalkan <esalkan@icloud.com>
2021-11-27 19:18:11 +0300

    Create _03_timer.java

---
+++ src/core/reviewsA/week_01/_03_timer.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package core.reviewsA.week_01;
+
+import java.util.Scanner;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _03_timer {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("please enter the number of minutes");
+        int minutes=scan.nextInt();
+        scan.close();
+        
+        for (int i=minutes; i > 0 ;--i) {
+            if( i< 0 ) {
+                break;
+            }
+            for (int z=59; z >=0 ; --z) {
+                System.out.print("\r"+ (i-1)+" minutes and "+z+" seconds left");
+                Thread.sleep(1000);  // pauses the execution of the code for given mill seconds
+            }
+        }
+
+        System.out.println("\n\n");
+
+        System.err.println("\t\t\t* * * * * * * * * * * * * * * * * * * * * * * * * *");
+        System.err.println("\t\t\t*                                                 *");
+        System.err.println("\t\t\t*       Times is Up, Please take your seats!      *");
+        System.err.println("\t\t\t*                                                 *");
+        System.err.println("\t\t\t* * * * * * * * * * * * * * * * * * * * * * * * * *");
+	}
+
+}
