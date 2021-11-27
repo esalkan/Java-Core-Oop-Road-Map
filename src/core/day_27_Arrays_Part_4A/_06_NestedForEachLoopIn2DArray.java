7ffbfa4100ecc2714dbdda557e12f7a966853a9c
esalkan <esalkan@icloud.com>
2021-11-27 19:09:23 +0300

    Create _06_NestedForEachLoopIn2DArray.java

---
+++ src/core/day_27_Arrays_Part_4A/_06_NestedForEachLoopIn2DArray.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package core.day_27_Arrays_Part_4A;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _06_NestedForEachLoopIn2DArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[][] staff = {
+				{"Ali", "Cihan", "Şamil", "Doğukan"},
+				{"Metin", "Semih", "Eray"},
+				{"Engin", "Naim", "Hüsnü","Efraim","Ali"}
+		};
+		
+		for (String[] persons : staff) {
+			for (String person : persons) {
+				System.out.print(person + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
