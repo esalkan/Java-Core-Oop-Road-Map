8c7548370c721170cd1db4aef00e2810cae2b4e0
esalkan <esalkan@icloud.com>
2021-11-27 19:12:04 +0300

    Create Question_17.java

---
+++ src/core/day_29_Arrays_Part_6A/Lab_4_Cont/Question_17.java
@@ -0,0 +1,56 @@
+package core.day_29_Arrays_Part_6A.Lab_4_Cont;
+
+import java.util.Arrays;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Question_17 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * Write a method that accepts an array and print the difference between the
+		 * largest and smallest values in the array.
+		 * 
+		 * int[] x = {10,3,5,6} = > 7
+		 * 
+		 * int[] y = {7,2,10,9} = > 8
+		 * 
+		 * int[] z = {2,10,7,2} = > 8
+		 */
+
+		int[] x = { 10, 3, 5, 6 };
+		System.out.println(difference(x));
+	}
+
+	public static int difference(int[] arr) {
+		// First Solution
+		int result = 0;
+		Arrays.sort(arr);
+		result = arr[arr.length - 1] - arr[0];
+		return result;
+		
+		// Second Alternative Solution
+//		int max = 0;
+//		for (int i = 0; i < arr.length; i++) {
+//			if (arr[i] > max) {
+//				max = arr[i];
+//			}
+//		}
+//		
+//		int min = max;
+//		for (int i = 0; i < arr.length; i++) {
+//			if (arr[i] < min) {
+//				min = arr[i];
+//			}
+//		}
+//		
+//		
+//		return max-min;
+	}
+
+}
