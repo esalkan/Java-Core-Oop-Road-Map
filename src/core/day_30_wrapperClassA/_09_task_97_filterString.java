b1ef1c7fa054d3707daa1d823bb5207121306b31
esalkan <esalkan@icloud.com>
2021-11-27 19:13:40 +0300

    Create _09_task_97_filterString.java

---
+++ src/core/day_30_wrapperClassA/_09_task_97_filterString.java
@@ -0,0 +1,37 @@
+package core.day_30_wrapperClassA;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _09_task_97_filterString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		String str = "(((W&e**_lco73me %t%o co!@$,,<r>e <J>>>$@av```~a777.||}{";
+
+		System.out.println(filterString(str));
+
+	}
+
+	public static String filterString(String s) {
+
+		String str = "";
+
+		for (int i = 0; i < s.length(); i++) {
+
+			if (Character.isAlphabetic(s.charAt(i)) || s.charAt(i) == ' ') {
+
+				str = str + s.charAt(i);
+			}
+
+		}
+
+		return str;
+
+	}
+
+}
