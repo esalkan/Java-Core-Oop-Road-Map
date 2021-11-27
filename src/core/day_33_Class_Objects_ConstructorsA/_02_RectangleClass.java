9d88535e02f4cce4765448cdab9c500e480417a7
esalkan <esalkan@icloud.com>
2021-11-27 19:17:21 +0300

    Create _02_RectangleClass.java

---
+++ src/core/day_33_Class_Objects_ConstructorsA/_02_RectangleClass.java
@@ -0,0 +1,19 @@
+package core.day_33_Class_Objects_ConstructorsA;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _02_RectangleClass {
+
+	// Creating variables.
+	// Their default value is 0.00
+	double length;
+	double width;
+	
+	// Creating a Method without return without parameter
+	public void getArea() {
+		// Just print what we send the print method
+		System.out.println(length * width);
+	}
+}
