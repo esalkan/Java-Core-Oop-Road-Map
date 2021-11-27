7cc42e718d1799729adf1bc22a985e0e545eccc2
esalkan <esalkan@icloud.com>
2021-11-27 19:19:12 +0300

    Create assignmentOperatorsReview.java

---
+++ src/core/reviewsA/week_02/assignmentOperatorsReview.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package core.reviewsA.week_02;
+
+import java.util.Scanner;
+
+/**
+ * @author esalkan
+ *
+ */
+public class assignmentOperatorsReview {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 1. Create a scanner object and import
+		Scanner input = new Scanner(System.in);
+		// 2. Prompt user
+		System.out.println(" Provide how much you make hourly: ");
+
+		// 3. Pick the value from console
+
+		int hourlyRate = input.nextInt(); // the part that you need to know right now
+
+		int weeklyHours = 40;
+
+		int monthlyTax = 200;
+
+		int monthlyPayCheck = 0;
+		// how much I make monthly after Tax, use shorthand operators
+		// without Tax
+		monthlyPayCheck += weeklyHours * hourlyRate;
+
+		monthlyPayCheck *= 4;
+
+		monthlyPayCheck -= monthlyTax;
+
+		System.out.println("Your salary monthly after Tax " + monthlyPayCheck);
+	}
+
+}
