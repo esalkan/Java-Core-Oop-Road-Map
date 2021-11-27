cd6b1b2f7a17641c698abce137d344085f0235c9
esalkan <esalkan@icloud.com>
2021-11-27 18:11:13 +0300

    Update _11_passArguments.java

--- src/core/day_13_methods_Part_1/_11_passArguments.java
+++ src/core/day_13_methods_Part_1/_11_passArguments.java
@@ -9,13 +9,19 @@ public class _11_passArguments {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		double x = 10.45;
-		displayValue(10);
+		
+		// displayValue(10);
+		
 		displayValue((int) x);
+		
 		// displayValue(x*4);
 	}
 
 	public static void displayValue(int num1) {
+		
 		System.out.println("The value is " + num1);
+		
 	}
 }
