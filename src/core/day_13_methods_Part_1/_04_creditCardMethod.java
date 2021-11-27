9d51218ad8562c9c373ddb0200953fa2d043f30b
esalkan <esalkan@icloud.com>
2021-11-27 23:06:37 +0300

    Update _04_creditCardMethod.java

--- src/core/day_13_methods_Part_1/_04_creditCardMethod.java
+++ src/core/day_13_methods_Part_1/_04_creditCardMethod.java
@@ -11,22 +11,35 @@ public class _04_creditCardMethod {
 	public static void main(String[] args) {
 
 		double salary = 50000;
-		int creditRating = 5;
 
+		int creditRating = 5;
+			//    true      &&   false = false
 		if (salary >= 20000 && creditRating >= 7) {
+
 			qualify();
+
 		} else {
+
 			noQualify();
+
 		}
 
 	}
 
 	public static void noQualify() {
-		System.out.println("I am sorry, you are not qualified for the CC ");
+		
+		int a = 1;
+		int b = 5;
+		int result = a + b ;
+
+		System.out.println("I am sorry, you are not qualified for the CC " + result);
+
 	}
 
 	public static void qualify() {
+
 		System.out.println("Congrat, you are qualified for the CC ");
+
 	}
 
 }
