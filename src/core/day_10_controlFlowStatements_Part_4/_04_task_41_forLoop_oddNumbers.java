16bfbcb24e1c9351fd9e15d6b34ea4217c6b0b47
esalkan <esalkan@icloud.com>
2021-11-27 18:10:41 +0300

    Update _04_task_41_forLoop_oddNumbers.java

--- src/core/day_10_controlFlowStatements_Part_4/_04_task_41_forLoop_oddNumbers.java
+++ src/core/day_10_controlFlowStatements_Part_4/_04_task_41_forLoop_oddNumbers.java
@@ -9,13 +9,13 @@ public class _04_task_41_forLoop_oddNumbers {
 	/**
 	 * @param args
 	 */
-
+	// odd = tek sayılar
 	// Write a program that displays all odd numbers between 5-140 in same line
 
 	public static void main(String[] args) {
 
 		for (int i = 5; i <= 140; i++) {
-			if (i % 2 != 1) {
+			if (i % 2 == 1) {
 				System.out.print(i + ", ");
 			}
 		}
