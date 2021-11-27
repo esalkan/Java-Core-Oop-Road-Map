2d67456419af46a221cf4526386bd6b3cd30bedb
esalkan <esalkan@icloud.com>
2021-11-27 23:06:39 +0300

    Update _11_valueReturn.java

--- src/core/day_14_methods_Part_2/_11_valueReturn.java
+++ src/core/day_14_methods_Part_2/_11_valueReturn.java
@@ -9,13 +9,11 @@ public class _11_valueReturn {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int x = sum(2, 3);
-		System.out.println(x);
+		
+		
+		
+
 	}
 
-	public static int sum(int num1, int num2) {
-		int result; // local variable
-		result = num1 + num2;
-		return result;
-	}
+
 }
