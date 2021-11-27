74c29ec07560f3a7bb65c6ca54b54bf25082ffe6
esalkan <esalkan@icloud.com>
2021-11-27 23:07:10 +0300

    Create _10_salary.java

---
+++ src/core/reviewsA/week_05/_10_salary.java
@@ -0,0 +1,36 @@
+package core.reviewsA.week_05;
+
+/**
+ * @author esalkan
+ */
+public class _10_salary {
+	// attributes : rate (how much you make an hour) 45 - 75 an hour / 15 dolars
+	// weeklyHour (int) , taxRate
+	// actions: setInfo() - > initilize your variables
+	// salary : rate * weeklyHour * 52
+	// totalTax : salary*taxRate
+	// salaryAfterTax()
+
+	// instance variables: global, you can use at every method
+	double rate;
+	int weeklyHour;
+	double taxRate;
+
+	public void setInfo(double r, int h, double t) { // r, h, t are local variables
+		rate = r;
+		weeklyHour = h;
+		taxRate = t;
+	}
+
+	public double salary() { // instance methods
+		return rate * weeklyHour * 52;
+	}
+
+	public double totalTax() { // instance methods
+		return salary() * taxRate;
+	}
+
+	public double salaryAfterTax() { // instance methods
+		return salary() - totalTax();
+	}
+}
