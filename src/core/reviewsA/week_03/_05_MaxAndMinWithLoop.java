707bd817a8c4f8042cd1eee469d1d0916a9901b1
esalkan <esalkan@icloud.com>
2021-11-27 19:20:11 +0300

    Create _05_MaxAndMinWithLoop.java

---
+++ src/core/reviewsA/week_03/_05_MaxAndMinWithLoop.java
@@ -0,0 +1,42 @@
+package core.reviewsA.week_03;
+
+import java.util.Scanner;
+
+/**
+ * @author esalkan
+ */
+public class _05_MaxAndMinWithLoop {
+
+	/**
+	 * @param args
+	 */
+	// You will get n (5) numbers of input from user
+		// you print out max and min numbers of those entries
+		
+		public static void main(String[] args) {
+			
+			// to get input from user we scanner class
+			// create scanner object
+			
+			Scanner input = new Scanner (System.in);
+			System.out.println("Enter a number");
+			int first = input.nextInt();
+			
+			int max = first;
+			int min = first;
+		
+			for(int i = 1 ; i<=4 ; i++) {
+				System.out.println("Enter another number");
+				int number = input.nextInt();
+				if (number>max) {
+					max=number;
+				}
+				if(number<min) {
+					min=number;
+				}
+			}
+			System.out.println("Max numer : " + max);
+			System.out.println("Min numer : " + min);
+		}
+
+}
