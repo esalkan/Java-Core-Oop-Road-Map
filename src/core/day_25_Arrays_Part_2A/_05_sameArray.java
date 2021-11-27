f58fac9bdd982dbfef55b3a185552b8126803bda
esalkan <esalkan@icloud.com>
2021-11-27 19:08:53 +0300

    Create _05_sameArray.java

---
+++ src/core/day_25_Arrays_Part_2A/_05_sameArray.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package core.day_25_Arrays_Part_2A;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _05_sameArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] arry1 = { 1, 3, 5, 6, 7, 8, 9 };
+		int[] arry2 = arry1;
+
+		arry1[0] = 300;
+		arry2[1] = 1453;
+
+		System.out.println("The Content Of Array 1 : ");
+		for (int i : arry1) {
+			System.out.print(i + " ");
+		}
+		
+		
+		System.out.println("\nThe Content Of Array 2 : ");
+
+		for (int i : arry2) {
+			System.out.print(i + ",");
+		}
+	}
+}
