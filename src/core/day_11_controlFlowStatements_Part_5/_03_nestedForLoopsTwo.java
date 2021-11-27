7093ea381f8c7cae95717e4480bdfafba806ec81
esalkan <esalkan@icloud.com>
2021-11-27 18:10:48 +0300

    Update _03_nestedForLoopsTwo.java

--- src/core/day_11_controlFlowStatements_Part_5/_03_nestedForLoopsTwo.java
+++ src/core/day_11_controlFlowStatements_Part_5/_03_nestedForLoopsTwo.java
@@ -12,12 +12,17 @@ public class _03_nestedForLoopsTwo {
 	public static void main(String[] args) {
 
 		for (int i = 0; i < 3; i++) {
+
 			System.out.println("Outter Loop at state : " + i);
+
 			for (int p = 0; p <= 3; p++) {
+
 				System.out.println("Inner loop at state : " + p);
 
 				if (p == 3) {
+
 					System.out.println();
+
 				}
 			}
 		}
