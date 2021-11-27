253dae05b44fde31e9df250067b7d7eb20877932
esalkan <esalkan@icloud.com>
2021-11-27 19:13:27 +0300

    Create _04_mathClass.java

---
+++ src/core/day_30_wrapperClassA/_04_mathClass.java
@@ -0,0 +1,32 @@
+package core.day_30_wrapperClassA;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _04_mathClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// System language class
+		
+		double result = Math.pow(2, 2);
+		System.out.println(result);
+		
+		double result2 = Math.sqrt(5);
+		System.out.println(result2);
+		
+		final double pi = Math.PI;
+		System.out.println(pi);
+
+		
+		System.out.println(Math.round(pi));
+		System.out.println(Math.floor(pi));
+		System.out.println(Math.ceil(pi));
+		
+		
+	}
+
+}
