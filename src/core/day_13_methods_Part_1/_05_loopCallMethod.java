95377ba37b1a12e65acacb6eb5c05053c1bd019b
esalkan <esalkan@icloud.com>
2021-11-27 18:11:11 +0300

    Update _05_loopCallMethod.java

--- src/core/day_13_methods_Part_1/_05_loopCallMethod.java
+++ src/core/day_13_methods_Part_1/_05_loopCallMethod.java
@@ -9,10 +9,14 @@ public class _05_loopCallMethod {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		System.out.println("hello from the main method");
+		
 		for (int i = 0; i < 5; i++) {
+		
 			displayMessage();
-			System.out.println("Back in the main method");
+			
+			System.out.println("Back in the for loop");
 		}
 
 	}
