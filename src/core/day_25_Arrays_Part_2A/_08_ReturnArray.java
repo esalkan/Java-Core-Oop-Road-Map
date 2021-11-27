eb815982cd144781e3674cf45529a60a11600324
esalkan <esalkan@icloud.com>
2021-11-27 18:11:42 +0300

    Create _08_ReturnArray.java

---
+++ src/core/day_25_Arrays_Part_2A/_08_ReturnArray.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package core.day_25_Arrays_Part_2A;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _08_ReturnArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// for (double num : getArray()) {
+		// 		System.out.println(num);
+		//	}
+		
+		double[] values = getArray();
+		// or 
+		// double[] values;
+		// values = getArray();
+		for (double d : values) {
+			System.out.println(d);
+		}
+
+	}
+
+	public static double[] getArray() {
+
+		double[] array = { 1.2, 2.3, 3.4, 4.5, 5.6 };
+
+		return array;
+
+	}
+
+}
