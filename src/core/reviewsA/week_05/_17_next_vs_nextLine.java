e92f3a58bdc988a458a28f89969dc489ebbd5637
esalkan <esalkan@icloud.com>
2021-11-27 23:07:23 +0300

    Create _17_next_vs_nextLine.java

---
+++ src/core/reviewsA/week_05/_17_next_vs_nextLine.java
@@ -0,0 +1,37 @@
+package core.reviewsA.week_05;
+
+import java.util.Scanner;
+
+/**
+ * @author esalkan
+ */
+public class _17_next_vs_nextLine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter your first Name");
+
+		String firstName = input.next();
+
+		System.out.println("Enter your Last Name");
+		String lastName = input.next();
+
+		System.out.println("Enter your age");
+		short age = input.nextShort();
+
+		input.nextLine(); // extra for accepting that ENTER button from previous step
+
+		System.out.println("Enter your addres");
+		String address = input.nextLine();
+
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(age);
+		System.out.println(address);
+	}
+
+}
