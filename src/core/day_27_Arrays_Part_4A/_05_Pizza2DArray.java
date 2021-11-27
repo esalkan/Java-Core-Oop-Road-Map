a4d6be69499142c21216f661ffee2b1cda797754
esalkan <esalkan@icloud.com>
2021-11-27 19:09:19 +0300

    Create _05_Pizza2DArray.java

---
+++ src/core/day_27_Arrays_Part_4A/_05_Pizza2DArray.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package core.day_27_Arrays_Part_4A;
+
+import java.util.Arrays;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _05_Pizza2DArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[][] pizzas = {
+				{ "pineapple", "pepperoni" },
+				{ "anchovies", "mushroom", "olives" },
+				{ "4 Cheese" },
+				{ "chicken", "tomatoes", "onions" },
+				{ "green peppers", "zuccini", "brocoli", "spinach", "shripm"}
+		};
+		
+		// System.out.println(Arrays.toString(pizzas[0]));
+		
+		for (String[] pizza : pizzas) {
+			System.out.print(pizza.length + "-" );
+			System.out.println(Arrays.toString(pizza));
+		}
+		
+		System.out.println("\n+----- print pizzas with for iterator loop -----+");
+		
+		for (int i = 0; i < pizzas.length; i++) {
+			for (int j = 0; j < pizzas[i].length; j++) {
+				System.out.print(pizzas[i][j] + " - ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\n+----- pizzas 3th array print -----+");
+		
+		for (String pizza : pizzas[3]) {
+			System.out.print (pizza + " ");
+		}
+		
+	}
+
+}
