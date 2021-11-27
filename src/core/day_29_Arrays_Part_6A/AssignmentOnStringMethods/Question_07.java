d20628954ecc52ae3c83c1ed69dfe3bb9d427d7e
esalkan <esalkan@icloud.com>
2021-11-27 19:11:45 +0300

    Create Question_07.java

---
+++ src/core/day_29_Arrays_Part_6A/AssignmentOnStringMethods/Question_07.java
@@ -0,0 +1,38 @@
+package core.day_29_Arrays_Part_6A.AssignmentOnStringMethods;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Question_07 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * 
+		 * Write a program that will print out the first half of the word twice.
+		 * 
+		 * Sample Output:
+		 * 
+		 * input: java output: jaja
+		 */
+
+		String str = "java";
+		
+		int strHalf = str.length()/2;
+		
+		String temp = "";
+		
+		for (int i = 0; i < strHalf; i++) {
+			temp += str.charAt(i);
+		}
+		
+		for (int i = 0; i < 2; i++) {
+			System.out.print(temp);
+		}
+		
+	}
+
+}
