fed21b9bc95c0e1171a6a50e2f348c483313e469
esalkan <esalkan@icloud.com>
2021-11-27 19:19:03 +0300

    Create end_of_the_week_Notes.java

---
+++ src/core/reviewsA/week_01/end_of_the_week_Notes.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package core.reviewsA.week_01;
+
+/**
+ * @author esalkan
+ *
+ */
+public class end_of_the_week_Notes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
