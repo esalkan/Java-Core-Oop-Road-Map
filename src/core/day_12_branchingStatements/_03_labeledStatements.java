864931ea50813170b205070f1545757993621ae9
esalkan <esalkan@icloud.com>
2021-11-27 18:10:54 +0300

    Update _03_labeledStatements.java

--- src/core/day_12_branchingStatements/_03_labeledStatements.java
+++ src/core/day_12_branchingStatements/_03_labeledStatements.java
@@ -10,14 +10,23 @@ public class _03_labeledStatements {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
 		int i = 0;
+
 		outer: do {
+
 			i = 8;
+
 			inner: while (true) {
+
 				System.out.println(i--);
+
 				if (i == 4) {
+
 					break outer;
+
 				}
+
 			}
 		} while (true);
 	}
