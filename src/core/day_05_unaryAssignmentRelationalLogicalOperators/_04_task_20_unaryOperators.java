3ca587a6d2ff6ae582ba5aa2bbe8f43c753323bc
esalkan <esalkan@icloud.com>
2021-11-27 22:44:46 +0300

    Update _04_task_20_unaryOperators.java

--- src/core/day_05_unaryAssignmentRelationalLogicalOperators/_04_task_20_unaryOperators.java
+++ src/core/day_05_unaryAssignmentRelationalLogicalOperators/_04_task_20_unaryOperators.java
@@ -10,7 +10,6 @@ public class _04_task_20_unaryOperators {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int a = 1;
 		a = -a-- + a++ / -a-- * --a;
