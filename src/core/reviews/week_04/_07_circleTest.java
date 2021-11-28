40c8d351c544a7c69275576842cf019a497f797a
esalkan <esalkan@icloud.com>
2021-11-28 19:54:34 +0300

    Create _07_circleTest.java

---
+++ src/core/reviews/week_04/_07_circleTest.java
@@ -0,0 +1,34 @@
+package core.reviews.week_04;
+
+/**
+ * @author esalkan
+ */
+public class _07_circleTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		Syntax:   ClassName objectName = new ClassName();
+
+		_06_circle circle1 = new _06_circle();
+
+		System.out.println(circle1.radius);
+
+		circle1.radius = 5.0;
+		System.out.println(circle1.area()); // I call the methods with the object name
+		System.out.println(circle1.perimeter());
+
+		_06_circle circle2 = new _06_circle();
+		circle2.radius = 10.0;
+		System.out.println(circle2.area());
+		System.out.println(circle2.perimeter());
+
+		System.out.println("-----------------------------");
+		// System.out.println(circle1); // the address of the reference variable in the
+		// Stack Memory
+
+		System.out.println(circle1.toString());
+	}
+
+}
