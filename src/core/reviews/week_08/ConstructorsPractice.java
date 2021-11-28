13adef29c4aeaae6ab30bbc5a7bcbc784b01452f
esalkan <esalkan@icloud.com>
2021-11-28 22:59:49 +0300

    Create ConstructorsPractice.java

---
+++ src/core/reviews/week_08/ConstructorsPractice.java
@@ -0,0 +1,33 @@
+package core.reviews.week_08;
+
+/**
+ * @author esalkan
+ *
+ */
+public class ConstructorsPractice {
+	/*
+	 * no paramater constructor one arg with int constructor one arg with string
+	 * constructor
+	 */
+
+	public ConstructorsPractice() {
+		this(10);
+		System.out.println("a");
+	}
+
+	public ConstructorsPractice(int num) { // call no arg constructor from here
+
+		System.out.println("b");
+	}
+
+	public ConstructorsPractice(String str) {
+		this();
+		System.out.println("c");
+	}
+
+	public static void main(String[] args) {
+
+		new ConstructorsPractice("hello"); // I am creating an object, I am calling constructor
+
+	}
+}
