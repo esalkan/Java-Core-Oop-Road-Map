d20e458406e1a7fcc027c7a8841133e78cd78f3a
esalkan <esalkan@icloud.com>
2021-11-28 19:54:52 +0300

    Create _12_local_Vs_Instance_Variables.java

---
+++ src/core/reviews/week_04/_12_local_Vs_Instance_Variables.java
@@ -0,0 +1,13 @@
+package core.reviews.week_04;
+
+/**
+ * @author esalkan
+ */
+public class _12_local_Vs_Instance_Variables {
+	int a; // instance variable
+
+	public void showDifference() {
+		int a = 5; // local variable
+		System.out.println(a);
+	}
+}
