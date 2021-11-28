f27115a2048849bf2e8a846c1daa1ab6ce19ae70
esalkan <esalkan@icloud.com>
2021-11-28 22:59:50 +0300

    Create Credentials.java

---
+++ src/core/reviews/week_08/Credentials.java
@@ -0,0 +1,52 @@
+package core.reviews.week_08;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Credentials {
+	private String userName = "Spartan300";
+	private String passWord = "hectorTheTroyan";
+
+	// I have a default constructor
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+// implement password logic: more than 8 characters, at least one Uppercase, lowercase and special
+	public void setPassWord(String passWord) {
+		boolean passwordSize;
+		boolean uppercase = false;
+		boolean lowercase = false;
+		boolean special = false;
+
+		passwordSize = (passWord.length() > 8) ? true : false;
+		for (char eachLetter : passWord.toCharArray()) {
+			if (Character.isUpperCase(eachLetter)) {
+				uppercase = true;
+			}
+			if (Character.isLowerCase(eachLetter)) {
+				lowercase = true;
+			}
+			if (!Character.isLetterOrDigit(eachLetter)) {
+				special = true;
+			}
+		}
+
+		if (passwordSize && uppercase && lowercase && special) {
+			this.passWord = passWord;
+		} else {
+			this.passWord = null;
+		}
+
+	}
+}
