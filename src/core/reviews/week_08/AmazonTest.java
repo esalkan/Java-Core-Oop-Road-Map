c831c907760198a7eb5e5454eeec13baf7790520
esalkan <esalkan@icloud.com>
2021-11-28 22:59:47 +0300

    Create AmazonTest.java

---
+++ src/core/reviews/week_08/AmazonTest.java
@@ -0,0 +1,46 @@
+package core.reviews.week_08;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * @author esalkan
+ *
+ */
+public class AmazonTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ArrayList<Item> items = new ArrayList<>();
+
+		items.add(new Item("Sony Vaio i7", 950.0, 10));
+		items.add(new Item("Lenove", 800.0, 20));
+		items.add(new Item("LG Ultrawide monitor", 450.0, 13));
+		items.add(new Item("HP Laptop", 750.0, 24));
+
+		// the value of my inventory
+		double valueOfInventory = 0;
+		// largest quantity of product
+		int largestQuantity = 0;
+		String nameOfLargest = "";
+
+		ArrayList<Integer> quantities = new ArrayList<>();
+		for (Item each : items) {
+			valueOfInventory += each.calCost();
+			if (each.quantity > largestQuantity) {
+				largestQuantity = each.quantity;
+				nameOfLargest = each.name;
+			}
+			quantities.add(each.quantity);
+		}
+
+		System.out.println("Value of inventory :" + valueOfInventory);
+		System.out.println("Item with largest amount : " + nameOfLargest + " and quatity : " + largestQuantity);
+
+		System.out.println(Collections.max(quantities));
+
+	}
+
+}
