a04250353cfe23762da39e22fb52035686115a24
esalkan <esalkan@icloud.com>
2021-11-28 22:59:55 +0300

    Create Login.java

---
+++ src/core/reviews/week_08/Login.java
@@ -0,0 +1,29 @@
+package core.reviews.week_08;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Login {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Credentials credentials = new Credentials();
+
+//	System.out.println(credentials.userName); Not reachable
+
+//credentials.passWord = "SPARTAAAAAA"; Not reachable
+
+		System.out.println(credentials.getUserName());
+		System.out.println(credentials.getPassWord());
+
+		credentials.setPassWord("Neb&uchadnezzar324");
+
+		System.out.println("After changing : " + credentials.getPassWord());
+
+	}
+
+}
