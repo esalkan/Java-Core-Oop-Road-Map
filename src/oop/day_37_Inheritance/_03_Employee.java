d60198ac7fa2810ee58b5079f3e2d9d77323c927
esalkan <esalkan@icloud.com>
2021-11-30 15:55:25 +0300

    Update _03_Employee.java

--- src/oop/day_37_Inheritance/_03_Employee.java
+++ src/oop/day_37_Inheritance/_03_Employee.java
@@ -9,12 +9,16 @@ public class _03_Employee extends _01_Person {
 	String jobTitle;
 	
 	public void work() {
+		
 		System.out.println(name + " is working as " + jobTitle);
+		
 	}
 	
 	@Override
 	public String toString() {
+		
 		return "Employee [jobTitle=" +  jobTitle + " , name=" + name + ", age=" + age + ", gender=" + gender + "]";
+		
 	}
 	
 	
