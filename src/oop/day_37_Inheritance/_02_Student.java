293fabf3a92ed7dbd038359067afb0cb05fddefa
esalkan <esalkan@icloud.com>
2021-12-04 17:08:32 +0300

    Update _02_Student.java

--- src/oop/day_37_Inheritance/_02_Student.java
+++ src/oop/day_37_Inheritance/_02_Student.java
@@ -6,21 +6,21 @@ package oop.day_37_Inheritance;
  */
 
 // That class extending from Parent Class and it can use the parent class methods and variables
-public class _02_Student extends _01_Person{
-	
+public class _02_Student extends _01_Person {
+
 	int studentId;
 	String termClass;
-	
+
 	public void code(String lang) {
-	
+
 		System.out.println(name + " is coding " + lang);
-	
+
 	}
-	
+
 	public void attendClass() {
-	
+
 		System.out.println(name + " is attending " + termClass + " class");
-	
+
 	}
-	
+
 }
