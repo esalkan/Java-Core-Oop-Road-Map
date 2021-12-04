cb3603b717b46e3816a83ad4f60b539ba7c8ebaf
esalkan <esalkan@icloud.com>
2021-12-04 19:04:05 +0300

    Create _10_Abc.java

---
+++ src/oop/day_36_StaticClassMembers/_10_Abc.java
@@ -0,0 +1,21 @@
+package oop.day_36_StaticClassMembers;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _10_Abc {
+
+	private _10_Abc() {
+
+	}
+
+	public static void m1() {
+
+	}
+
+	public static void m2() {
+
+	}
+
+}
