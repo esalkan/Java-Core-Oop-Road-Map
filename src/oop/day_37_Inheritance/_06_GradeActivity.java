4bfc78bdf8b141be230c39da88099660d5a13bba
esalkan <esalkan@icloud.com>
2021-12-04 17:08:36 +0300

    Update _06_GradeActivity.java

--- src/oop/day_37_Inheritance/_06_GradeActivity.java
+++ src/oop/day_37_Inheritance/_06_GradeActivity.java
@@ -21,25 +21,25 @@ public class _06_GradeActivity {
 		char letterGrade;
 
 		if (score >= 90) {
-			
+
 			letterGrade = 'A';
-			
+
 		} else if (score >= 80) {
-			
+
 			letterGrade = 'B';
-			
+
 		} else if (score >= 70) {
-			
+
 			letterGrade = 'C';
-			
+
 		} else if (score >= 60) {
-			
+
 			letterGrade = 'D';
-			
+
 		} else {
-		
+
 			letterGrade = 'F';
-			
+
 		}
 
 		return letterGrade;
