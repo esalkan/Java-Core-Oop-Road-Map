7525831bac61920acf65baa0bad532f9406ebd38
esalkan <esalkan@icloud.com>
2021-12-04 17:08:28 +0300

    Create _02_StudentTest.java

---
+++ src/oop/day_36_StaticClassMembers/_02_StudentTest.java
@@ -0,0 +1,18 @@
+package oop.day_36_StaticClassMembers;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _02_StudentTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Maraba Zalım Dunya");
+
+	}
+
+}
