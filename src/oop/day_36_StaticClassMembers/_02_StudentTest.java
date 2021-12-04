7cbc45c2365cad1b69404fa675fb1723b6d59144
esalkan <esalkan@icloud.com>
2021-12-04 17:18:53 +0300

    Update _02_StudentTest.java

--- src/oop/day_36_StaticClassMembers/_02_StudentTest.java
+++ src/oop/day_36_StaticClassMembers/_02_StudentTest.java
@@ -10,8 +10,21 @@ public class _02_StudentTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
-		System.out.println("Maraba Zalım Dunya");
+		// Reaching directly static school variable which is assigned before Middle East
+		// Technical University
+		System.out.println(_01_Student.school.toString());
+		
+		// Changing Static variable value to AIBU
+		String justForPrint = _01_Student.school = "AIBU";
+		System.out.println(justForPrint);
+		
+		_01_Student std1 = new _01_Student("Aslı", 12, 1021);
+		_01_Student std2 = new _01_Student("Zümra", 11, 1020);
+		_01_Student std3 = new _01_Student("İpek", 6, 1019);
+		
+		System.out.println(std1.toString());
+		System.out.println(std2.toString());
+		System.out.println(std3.toString());
 
 	}
 
