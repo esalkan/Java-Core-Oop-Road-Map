48e0af2ae722122d8c96999f5d6a49b517b39f63
esalkan <esalkan@icloud.com>
2021-12-07 12:44:57 +0300

    Create _10_Employee.java

---
+++ src/oop/day_38_InheritanceContinue/_10_Employee.java
@@ -0,0 +1,13 @@
+package oop.day_38_InheritanceContinue;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _10_Employee {
+
+	// Creating a method
+	void calculatePay(int hours, double rate) {
+		System.out.println("Employee total pay : " + (hours * rate));
+	}
+}
