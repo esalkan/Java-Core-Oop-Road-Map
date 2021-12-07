1c48bab69f6b2da5170d5d10421622c575921337
esalkan <esalkan@icloud.com>
2021-12-07 12:44:55 +0300

    Create _09_StaticTest.java

---
+++ src/oop/day_38_InheritanceContinue/_09_StaticTest.java
@@ -0,0 +1,39 @@
+package oop.day_38_InheritanceContinue;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _09_StaticTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Creating an object from _08_StaticSub Class Constructor
+		_08_StaticSub obj = new _08_StaticSub();
+
+		// Reaching the 'public static num' instance variable from _07_StaticSuper Class
+		// Constructor and assign it a value.
+		obj.num = 5;
+
+		// Reaching print() Method from _08_StaticSub
+		obj.print();
+
+		// _08_StaticSub not have num variable. but _08_StaticSub is in a relationship
+		// with _07_StaticSuper and that num is come from there and that num is public
+		// static created
+		System.out.println(_08_StaticSub.num);
+
+		// m1() Method is public static thats why it is reachable
+		_08_StaticSub.m1();
+
+		// m2() Method is just public not static thats why we can not reach to it.
+		// _07_StaticSub.m2();
+
+		// From SubClass we can reach m2() method because it is extends from superclass
+		obj.m2();
+	}
+
+}
