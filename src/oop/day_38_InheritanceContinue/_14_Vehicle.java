aec5d9ac872abd2b1b59e770cb2dfe7dd16294fb
esalkan <esalkan@icloud.com>
2021-12-07 12:45:07 +0300

    Create _14_Vehicle.java

---
+++ src/oop/day_38_InheritanceContinue/_14_Vehicle.java
@@ -0,0 +1,12 @@
+package oop.day_38_InheritanceContinue;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _14_Vehicle {
+
+	// Create an instance Variable and assing the value
+	int maxSpeed = 120;
+
+}
