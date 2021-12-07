08238e5032c09cb5ac78c3d37af9af4d613c4991
esalkan <esalkan@icloud.com>
2021-12-07 12:45:10 +0300

    Create _16_CarTest.java

---
+++ src/oop/day_38_InheritanceContinue/_16_CarTest.java
@@ -0,0 +1,21 @@
+package oop.day_38_InheritanceContinue;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _16_CarTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Creating a c1 object from _15_Car Class
+		_15_Car c1 = new _15_Car();
+
+		// Call the _15_Car class display() Method
+		c1.display();
+	}
+
+}
