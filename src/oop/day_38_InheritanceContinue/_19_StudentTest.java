1447ceaaf4c46a49b8681da7435862cb0c29c9b1
esalkan <esalkan@icloud.com>
2021-12-07 12:45:17 +0300

    Create _19_StudentTest.java

---
+++ src/oop/day_38_InheritanceContinue/_19_StudentTest.java
@@ -0,0 +1,22 @@
+package oop.day_38_InheritanceContinue;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _19_StudentTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		_18_Student st = new _18_Student();
+		
+		st.display();
+		
+		st.message();
+		
+	}
+
+}
