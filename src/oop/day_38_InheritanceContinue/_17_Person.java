bb81889841b3c98860a1ba3896d17f73e7bd8a75
esalkan <esalkan@icloud.com>
2021-12-07 12:45:12 +0300

    Create _17_Person.java

---
+++ src/oop/day_38_InheritanceContinue/_17_Person.java
@@ -0,0 +1,12 @@
+package oop.day_38_InheritanceContinue;
+
+/**
+ * @author esalkan
+ *
+ */
+public class _17_Person {
+
+	public void message() {
+		System.out.println("This is person class");
+	}
+}
