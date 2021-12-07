4051b0a5c1d6ef4759023a6b3738cd4c5e1c16ba
esalkan <esalkan@icloud.com>
2021-12-07 12:44:39 +0300

    Create _01_SuperClass.java

---
+++ src/oop/day_38_InheritanceContinue/_01_SuperClass.java
@@ -0,0 +1,20 @@
+package oop.day_38_InheritanceContinue;
+
+/**
+ * @author esalkan
+ *
+ */
+
+// Creating a Constructor it is our Parent Class
+public class _01_SuperClass {
+
+	/**
+	 * In an inheritance relationship, the superclass constructor always executes
+	 * before the subclass constructor.
+	 */
+	
+	// Creating a Constructor with no parameter
+	public _01_SuperClass() {
+		System.out.println("This is the super class Constructur");
+	}
+}
