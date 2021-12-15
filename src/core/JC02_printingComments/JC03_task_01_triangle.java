307009273d5ae266c4ff2ae84f10c4d17d506bf0
esalkan <esalkan@icloud.com>
2021-12-15 21:13:08 +0300

    Create JC03_task_01_triangle.java

---
+++ src/core/JC02_printingComments/JC03_task_01_triangle.java
@@ -0,0 +1,19 @@
+package core.JC02_printingComments;
+/**
+ * @author esalkan
+ *
+ */
+public class JC03_task_01_triangle {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// print a rectangle with print method
+		System.out.println("    *");
+		System.out.println("   * * ");
+		System.out.println("  *   *");
+		System.out.println(" *     *");
+		System.out.println("*********");
+	}
+
+}
