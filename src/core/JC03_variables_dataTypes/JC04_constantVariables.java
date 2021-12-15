7bfdac15105fcfdd50a6adb797198250b6ac08fb
esalkan <esalkan@icloud.com>
2021-12-15 21:13:22 +0300

    Create JC04_constantVariables.java

---
+++ src/core/JC03_variables_dataTypes/JC04_constantVariables.java
@@ -0,0 +1,29 @@
+package core.JC03_variables_dataTypes;
+/**
+ * @author esalkan
+ *
+ */
+public class JC04_constantVariables {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		double PI = 3.14;
+		System.out.println(PI);
+		PI = 5;
+		System.out.println(PI);
+		PI = 100;
+		System.out.println(PI);
+
+		final double pi = 3.14;
+		System.out.println("final pi = " + pi);
+
+		final String birthday = "17/09/1981";
+		System.out.println("final string birthday " + birthday);
+
+		final int capacity = 50;
+		System.out.println("final int capacity = " + capacity);
+	}
+
+}
