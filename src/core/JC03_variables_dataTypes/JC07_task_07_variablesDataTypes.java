6071460fc37f1c553fc5dd125027735cb761b7aa
esalkan <esalkan@icloud.com>
2021-12-15 21:13:29 +0300

    Create JC07_task_07_variablesDataTypes.java

---
+++ src/core/JC03_variables_dataTypes/JC07_task_07_variablesDataTypes.java
@@ -0,0 +1,27 @@
+package core.JC03_variables_dataTypes;
+/**
+ * @author esalkan
+ *
+ */
+public class JC07_task_07_variablesDataTypes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int i1, i2;
+		float f1;
+		String s1;
+
+		i1 = 10;
+		i2 = 20;
+		f1 = 12.5f;
+		s1 = "Task-7";
+
+		System.out.println(i1);
+		System.out.println(i2);
+		System.out.println(f1);
+		System.out.println(s1);
+	}
+
+}
