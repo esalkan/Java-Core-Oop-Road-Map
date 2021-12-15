56b756900094639d07a26f643f223157af31a789
esalkan <esalkan@icloud.com>
2021-12-15 21:48:44 +0300

    updates

--- src/core/JavaCoreNotes.java
+++ src/core/JavaCoreNotes.java
@@ -1,9 +1,36 @@
 package core;
 
+import java.util.ArrayList;
+
 /**
  * @author esalkan
  *
  */
 public class JavaCoreNotes {
 
+	public static void main(String[] args) {
+		
+		
+		
+		for (String each : createVariables(10)) {
+			
+			
+		}
+		
+	}
+	
+	public static ArrayList<String> createVariables(int n){
+		ArrayList<String> variables = new ArrayList<String>();
+		
+		for(int i = 0; i <= n; i++) {
+			variables.add("variable"+i);
+		}
+		
+		
+		
+		return variables;
+	}
+	
 }
+
+
