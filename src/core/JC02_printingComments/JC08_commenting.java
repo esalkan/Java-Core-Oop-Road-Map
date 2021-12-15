6ac9695092d159763d7503836da69a207ca17a49
esalkan <esalkan@icloud.com>
2021-12-15 21:13:16 +0300

    Create JC08_commenting.java

---
+++ src/core/JC02_printingComments/JC08_commenting.java
@@ -0,0 +1,22 @@
+package core.JC02_printingComments;
+/**
+ * @author esalkan
+ *
+ */
+public class JC08_commenting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Author : ALKAN
+		// Time : 16:02
+		System.out.println("Hello World");
+
+		/*
+		 * Multiply Comment Line
+		 */
+
+	}
+
+}
