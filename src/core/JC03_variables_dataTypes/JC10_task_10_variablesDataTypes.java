3684ac62f6884013e8c95725efb383cfbd387408
esalkan <esalkan@icloud.com>
2021-12-15 21:14:09 +0300

    Create JC10_task_10_variablesDataTypes.java

---
+++ src/core/JC03_variables_dataTypes/JC10_task_10_variablesDataTypes.java
@@ -0,0 +1,23 @@
+package core.JC03_variables_dataTypes;
+/**
+ * @author esalkan
+ *
+ */
+public class JC10_task_10_variablesDataTypes {
+
+	/**
+	 * @param args
+	 */
+
+	// Which of the followings are legal declarations?
+
+	public static void main(String[] args) {
+
+		// boolean b1, b2;
+		// String s1= "1", s2;
+		// double d1,double d2;
+		// int i1; int i2;
+		// int i3; i4;
+	}
+
+}
