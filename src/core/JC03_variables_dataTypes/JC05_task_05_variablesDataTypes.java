ba556adb6c9d03510ff32f702ba972d8eea02575
esalkan <esalkan@icloud.com>
2021-12-15 21:13:24 +0300

    Create JC05_task_05_variablesDataTypes.java

---
+++ src/core/JC03_variables_dataTypes/JC05_task_05_variablesDataTypes.java
@@ -0,0 +1,20 @@
+package core.JC03_variables_dataTypes;
+/**
+ * @author esalkan
+ *
+ */
+public class JC05_task_05_variablesDataTypes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String name = "Eyüp Sabri ALKAN";
+		int age = 40;
+		boolean isStudent = true;
+
+		System.out.println("Student name is " + name + " & " + age + " years old." + " & " + "is he enroll the course? "
+				+ isStudent);
+	}
+
+}
