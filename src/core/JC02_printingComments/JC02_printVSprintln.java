d89499b12972d5821f7996abc83019cde01c9ab4
esalkan <esalkan@icloud.com>
2021-12-15 21:13:06 +0300

    Create JC02_printVSprintln.java

---
+++ src/core/JC02_printingComments/JC02_printVSprintln.java
@@ -0,0 +1,28 @@
+package core.JC02_printingComments;
+
+/**
+ * @author esalkan
+ *
+ */
+public class JC02_printVSprintln {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// println() is writing everything in a line and end of it put \n (new line end
+		// of it)
+		System.out.println("this is written with println() method");
+		// print() is writing everything in a line
+		System.out.print("this is written with print() method");
+		
+		// is giving us an empty new line like one <br> as in html :)
+		System.out.println();
+		
+		// if you switch place of the above codes you will see everything is written in one line
+		System.out.print("this is written with print() method");
+		System.out.println("this is written with println() method");
+
+	}
+
+}
