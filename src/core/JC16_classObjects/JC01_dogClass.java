5ac86fb03dc53a2e35e3267397f7cf567b790c68
esalkan <esalkan@icloud.com>
2021-12-15 21:12:58 +0300

    Create JC01_dogClass.java

---
+++ src/core/JC16_classObjects/JC01_dogClass.java
@@ -0,0 +1,24 @@
+package core.JC16_classObjects;
+
+/**
+ * @author esalkan
+ */
+public class JC01_dogClass {
+	String breed;
+	int age;
+	String color;
+	String name;
+
+	public void barking() {
+		System.out.println(name + " is barking.");
+	}
+
+	public void hungry() {
+		System.out.println(name + " is hungry.");
+	}
+
+	public void sleeping() {
+		System.out.println(name + " is sleeping.");
+	}
+
+}
