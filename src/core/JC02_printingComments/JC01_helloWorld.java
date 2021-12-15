11611bdb9c01c7b750581aace820241ca65c9518
esalkan <esalkan@icloud.com>
2021-12-15 21:13:03 +0300

    Create JC01_helloWorld.java

---
+++ src/core/JC02_printingComments/JC01_helloWorld.java
@@ -0,0 +1,18 @@
+package core.JC02_printingComments;
+/**
+ * @author esalkan
+ *
+ */
+public class JC01_helloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Print first message with JAVA
+		// print() println() methods will print what we give in it ;)
+		System.out.println("Maraba Zalım Dunya! :)");
+		System.out.println("Hello Cruel World! :)");
+	}
+
+}
