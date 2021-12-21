b9ee41a86632dd5e58b0a286c92b9dcaa0ec9a48
esalkan <esalkan@icloud.com>
2021-12-21 15:54:26 +0300

    Create Nokia.java

---
+++ src/oop/JOOP41_Abstraction/Practice/Nokia.java
@@ -0,0 +1,24 @@
+package oop.JOOP41_Abstraction.Practice;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Nokia extends MobilePhone {
+
+	// Nokia class is first child class which is abstract class MobilePhone. That's
+	// why need to add unimplemented methods from Parent abstract class.
+
+	// implementation methods from Parent Abstract Class. Because Nokia is extends
+	// MobilePhone
+	@Override
+	public void text() {
+		// Code
+	}
+
+	@Override
+	public void call() {
+		// Code
+	}
+
+}
