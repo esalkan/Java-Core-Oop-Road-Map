1a56a6f13ce651abba6833c6bfaadb9ce5071fb8
esalkan <esalkan@icloud.com>
2021-12-21 10:00:20 +0300

    Create Rodent.java

---
+++ src/oop/JOOP40_AccessModifiers_FinalKeyword_MethodHiding/Rodent.java
@@ -0,0 +1,15 @@
+package oop.JOOP40_AccessModifiers_FinalKeyword_MethodHiding;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Rodent {
+	
+	protected int tailLength = 4;
+	
+	public void getRodentDetails() {
+		System.out.println("parentTail : " + tailLength);
+	}
+
+}
