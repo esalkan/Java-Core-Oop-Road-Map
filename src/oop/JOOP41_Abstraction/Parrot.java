1686b98df286a20549b6c753bbdafac61963f06f
esalkan <esalkan@icloud.com>
2021-12-21 15:54:17 +0300

    Create Parrot.java

---
+++ src/oop/JOOP41_Abstraction/Parrot.java
@@ -0,0 +1,37 @@
+package oop.JOOP41_Abstraction;
+
+/**
+ * @author esalkan
+ *
+ */
+// RULE ...: If any abstract class "public abstract class Sparrow" extending
+// another abstrat class "extends Animal", this is optional implement(@Override)
+// abstract methods from extended class...
+public class Parrot extends Bird {
+
+	// Can we add constructor in abstract class? (Ex. Animal) - Yes we can.
+	// If we can not instantiate abstract class, how we can call the constructor?
+	public Parrot(String name) {
+		super(name);
+		/**
+		 * The constructor of abstract class can be called from a subclass using super
+		 * keyword.
+		 */
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+
+	}
+
+	// What method has an access?
+	// All of them from Bird and Animal Class because (extends) there is inheritance
+
+}
