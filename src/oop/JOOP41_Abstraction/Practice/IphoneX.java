0ac96bfcc29ce37949052cff00d20f78b40b140a
esalkan <esalkan@icloud.com>
2021-12-21 15:54:21 +0300

    Create IphoneX.java

---
+++ src/oop/JOOP41_Abstraction/Practice/IphoneX.java
@@ -0,0 +1,10 @@
+package oop.JOOP41_Abstraction.Practice;
+
+/**
+ * @author esalkan
+ *
+ */
+public class IphoneX extends Iphone {
+	// no required to override anything. because In Iphone Class is not abstract
+	// class and it can not have any abstract methods.
+}
