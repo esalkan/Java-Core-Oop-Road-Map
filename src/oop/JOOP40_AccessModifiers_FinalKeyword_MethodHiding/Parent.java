4d395d7e3b62e920d3251ff59c7ca3b612724946
esalkan <esalkan@icloud.com>
2021-12-21 10:00:22 +0300

    Create Parent.java

---
+++ src/oop/JOOP40_AccessModifiers_FinalKeyword_MethodHiding/Parent.java
@@ -0,0 +1,15 @@
+package oop.JOOP40_AccessModifiers_FinalKeyword_MethodHiding;
+
+/**
+ * @author esalkan
+ *
+ */
+
+public class Parent {
+	public int x = 100;
+	
+	public void increment() {
+		x++;
+	}
+	
+}
