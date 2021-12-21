7b1204c4057f84d06b4fffb6815ac5ffcc5f567a
esalkan <esalkan@icloud.com>
2021-12-21 10:00:11 +0300

    Create FinalClass.java

---
+++ src/oop/JOOP40_AccessModifiers_FinalKeyword_MethodHiding/FinalClass.java
@@ -0,0 +1,29 @@
+package oop.JOOP40_AccessModifiers_FinalKeyword_MethodHiding;
+
+/**
+ * @author esalkan
+ *
+ */
+
+/*
+ * final class
+ * 
+ * • When a class is declared with final keyword, it is called a final class. A
+ * final class cannot be extended(inherited).
+ * 
+ * • You can not make a class immutable without making it final.(String class)
+ */
+
+// if we put final access modifier to class as shown below. Sub class will complain. that is as explained above. 
+// Final class can not be extended (Inherited)
+final public class FinalClass {
+
+
+}
+
+/*
+class SubClass extends FinalClass {
+
+
+}
+*/
