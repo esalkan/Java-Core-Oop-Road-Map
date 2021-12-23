69ba4a95baa7afe8172fa7ae84f41c2a73e8c8d4
esalkan <esalkan@icloud.com>
2021-12-23 15:52:17 +0300

    Diamond shape composed of Numbers

---
+++ src/exercises/patternPrograms/Pattern04.java
@@ -0,0 +1,44 @@
+package exercises.patternPrograms;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Pattern04 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Diamond shape composed of Numbers
+
+		for (int i = 1; i <= 4; i++) {
+			int n = 4;
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = i; k >= 1; k--) {
+				System.out.print(k);
+			}
+			for (int l = 2; l <= i; l++) {
+				System.out.print(l);
+			}
+			System.out.println();
+		}
+		for (int i = 3; i >= 1; i--) {
+			int n = 3;
+			for (int j = 0; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = i; k >= 1; k--) {
+				System.out.print(k);
+			}
+			for (int l = 2; l <= i; l++) {
+				System.out.print(l);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
