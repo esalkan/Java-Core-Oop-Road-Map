4312bb048741f7694f94e2bdc04c88449e27888c
esalkan <esalkan@icloud.com>
2021-12-28 15:38:42 +0300

    Abstract class VS Interface

---
+++ src/oop/JOOP42_Interface/InterfaceSummary.java
@@ -0,0 +1,11 @@
+package oop.JOOP42_Interface;
+
+public class InterfaceSummary {
+
+    /*
+     Abstract Class VS Interface
+     - Abstract classes and Interfaces are used to achieve abstraction in JAVA
+
+     */
+
+}
