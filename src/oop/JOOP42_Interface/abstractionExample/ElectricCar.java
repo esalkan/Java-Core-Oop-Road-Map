a262dbbc970d4a38698263eb30cf29562c66b963
esalkan <esalkan@icloud.com>
2021-12-28 12:38:02 +0300

    Abstraction
     Example ElectricCar Abstract Class

---
+++ src/oop/JOOP42_Interface/abstractionExample/ElectricCar.java
@@ -0,0 +1,52 @@
+package oop.JOOP42_Interface.abstractionExample;
+
+/**
+ * @author esalkan
+ *
+ */
+public abstract class ElectricCar {
+	// Creating instance variables
+	private String model;
+	private double price;
+	private String color;
+	
+	// Creating Constructor
+	public ElectricCar(String model, double price, String color) {
+		this.model = model;
+		this.price = price;
+		this.color = color;
+	}
+	
+	// Creating abstract methods
+	public abstract void start(); // What TODO, instead of HOW TODO
+	public abstract void charge();
+	public abstract void drive();
+	
+	// Creating a regular method()
+	void stop() {
+		System.out.println("Electric car stopping by pushing the break...");
+	}
+
+	// Creating just GETTERS we will set them in the constructor.
+	public String getModel() {
+		return model;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	// Creating toString method for print 
+	@Override
+	public String toString() {
+		return "ElectricCar [model=" + model + ", price=" + price + ", color=" + color + "]";
+	}
+	
+	
+	
+	
+}
