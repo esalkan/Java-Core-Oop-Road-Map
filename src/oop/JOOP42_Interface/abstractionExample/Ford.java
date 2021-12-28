d41b212908457059e9332c08e671ade706dfb39f
esalkan <esalkan@icloud.com>
2021-12-28 12:41:07 +0300

    Abstract
    public class Ford extends ElectricCar

---
+++ src/oop/JOOP42_Interface/abstractionExample/Ford.java
@@ -0,0 +1,40 @@
+package oop.JOOP42_Interface.abstractionExample;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Ford extends ElectricCar {
+
+	private int year;
+
+	public Ford(String model, double price, String color, int year) {
+		super(model, price, color);
+		this.year = year;
+	}
+
+	// First Concreate Class Must implement all abstract methods.
+	@Override
+	public void start() {
+		System.out.println("Ford - Push start button to start...");
+	}
+
+	@Override
+	public void charge() {
+		System.out.println("Ford - Plugin to electric outlet...");
+	}
+
+	@Override
+	public void drive() {
+		System.out.println("Ford - is driving...");
+
+	}
+
+	/**
+	 * @return the year
+	 */
+	public int getYear() {
+		return year;
+	}
+
+}
