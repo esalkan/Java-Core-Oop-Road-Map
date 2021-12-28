005cff6730b64e9e9acdb1db73c3583d5bf5dcf2
esalkan <esalkan@icloud.com>
2021-12-28 15:43:16 +0300

    We can not instantiate abstract classes and interfaces

--- src/oop/JOOP42_Interface/InterfaceSummary.java
+++ src/oop/JOOP42_Interface/InterfaceSummary.java
@@ -5,6 +5,13 @@ public class InterfaceSummary {
     /*
      Abstract Class VS Interface
      - Abstract classes and Interfaces are used to achieve abstraction in JAVA
+     - We can not instantiate abstract classes and Interfaces
+            public abstract class Food{};
+            public Interface Cookable{};
+
+            Food food = new Food(); // Error
+            Cookable cook = new Cookable(); // Error
+
 
      */
 
