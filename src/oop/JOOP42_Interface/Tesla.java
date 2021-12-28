7d0bcd18757f79a9e98a744a4b7c85b48da52669
esalkan <esalkan@icloud.com>
2021-12-28 12:42:30 +0300

    Interface
    public class Tesla implements Electric

---
+++ src/oop/JOOP42_Interface/Tesla.java
@@ -0,0 +1,21 @@
+package oop.JOOP42_Interface;
+
+/**
+ * @author esalkan
+ *
+ */
+public class Tesla implements Electric {
+
+	// We have to impelement firstly unimplemented methods from Parent which is
+	// Electric Interface
+	@Override
+	public void charge() {
+	}
+
+	@Override
+	public void charge2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
