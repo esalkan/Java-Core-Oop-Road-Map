ea7374699e759c241a53548ed6ca0ef1069d3062
esalkan <esalkan@icloud.com>
2021-12-28 15:52:37 +0300

    - We use abstract classes for setting foundation for subclasses. It is normally general idea.

--- src/oop/JOOP42_Interface/InterfaceSummary.java
+++ src/oop/JOOP42_Interface/InterfaceSummary.java
@@ -5,13 +5,14 @@ public class InterfaceSummary {
     /*
      Abstract Class VS Interface
      - Abstract classes and Interfaces are used to achieve abstraction in JAVA
+
      - We can not instantiate abstract classes and Interfaces
             public abstract class Food{};
             public Interface Cookable{};
 
             Food food = new Food(); // Error
             Cookable cook = new Cookable(); // Error
-
+      - We use abstract classes for setting foundation for subclasses. It is normally general idea.
 
      */
 
