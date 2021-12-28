27a94ef30265c5390184ec65c5a4ad965d981b96
esalkan <esalkan@icloud.com>
2021-12-28 17:52:32 +0300

    Inheritance in Abstract classes and Interface

--- src/oop/JOOP42_Interface/InterfaceSummary.java
+++ src/oop/JOOP42_Interface/InterfaceSummary.java
@@ -98,8 +98,8 @@ public class InterfaceSummary {
         NOTE : Abstract Class can have everything regular class has + Abstract class can have abstract method as well
 
         - VARIABLES :
-            - Abstract classes can have instance, static and final variables.
-            - Interfaces can oly have "public static final" variables
+            - Abstract classes  : can have instanced, static and final variables.
+            - Interfaces        : can oly have "public static final" variables
 
             Examples :
                 Abstract Class Variables Example:
@@ -117,8 +117,8 @@ public class InterfaceSummary {
                     }
 
        - Constructor :
-            - Abstract classes can have constructors.
-            - Interfaces can not have constructors.
+            - Abstract classes  : can have constructors.
+            - Interfaces        : can not have constructors.
 
             Examples :
                 - Abstract Example;
@@ -148,8 +148,8 @@ public class InterfaceSummary {
             }
 
        - Access Modifiers :
-            - Abstract classes : can use all access modifiers with variables and methods.
-            - Interfaces : can only use "public" access modifiers
+            - Abstract classes  : can use all access modifiers with variables and methods.
+            - Interfaces        : can only use "public" access modifiers
                 - Note : with JAVA-11 private final available too.
 
             Examples :
@@ -172,10 +172,10 @@ public class InterfaceSummary {
                     }
 
        - Inheritance :
-            - Abstract classes : can extend a single abstract or non-abstract class and implement interfaces
-            - Interfaces    : can extend multiple interfaces
-                              support multiple Inheritance
-                              can not extend classes
+            - Abstract classes  : can extend a single abstract or non-abstract class and implement interfaces
+            - Interfaces        : can extend multiple interfaces
+                                  support multiple Inheritance
+                                  can not extend classes
 
             public abstract class Food{}
             public Interface Friable{}
