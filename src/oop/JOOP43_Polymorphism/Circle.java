4a23f7200f4a236edc4ce7da13a383118c839b76
esalkan <esalkan@icloud.com>
2021-12-29 16:03:56 +0300

    Polymorphism - Parent child relationship and create objects polymorphic way. Shapes Example

---
+++ src/oop/JOOP43_Polymorphism/Circle.java
@@ -0,0 +1,11 @@
+package oop.JOOP43_Polymorphism;
+
+public class Circle extends Shape{
+
+    @Override
+    public void draw() {
+        // super.draw();
+        System.out.println("Drawing a Circle");
+    }
+
+}
---
+++ src/oop/JOOP43_Polymorphism/Shape.java
@@ -0,0 +1,8 @@
+package oop.JOOP43_Polymorphism;
+
+public class Shape {
+
+    public void draw(){
+        System.out.println("Drawing a shape");
+    }
+}
