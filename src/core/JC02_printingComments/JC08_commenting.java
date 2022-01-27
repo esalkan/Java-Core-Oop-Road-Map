e69c6f18760c933716be1cad2c6a576e5f05466b
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-01-27 09:50:45 +0300

    Türkçe yorum satırları eklendi

--- src/core/JC02_printingComments/JC08_commenting.java
+++ src/core/JC02_printingComments/JC08_commenting.java
@@ -11,11 +11,14 @@ public class JC08_commenting {
 	 */
 	public static void main(String[] args) {
 		// Author : ALKAN
+		// Tekli Yorum Satırı
 		// Time : 16:02
+		/* Tekli yorum satırı olarak kullanılabilir. */
 		System.out.println("Hello World");
 
 		/*
 		 * Multiply Comment Line
+		 * Çoklu yorum satırı
 		 */
 
 	}
