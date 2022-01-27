c2792b963d9b47c0cfe9d2e6c8f4cef286d81b2d
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-01-27 09:50:01 +0300

    Türkçe yorum satırları eklendi

--- src/core/JC02_printingComments/JC07_task_04_escapeSequences.java
+++ src/core/JC02_printingComments/JC07_task_04_escapeSequences.java
@@ -10,7 +10,7 @@ public class JC07_task_04_escapeSequences {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		// ESCAPE SEQUENCES Exercise - KAÇIŞ DİZELERİ Egzersiz
 		System.out.println("\"\\__/\"\n\nHe said \"Hello\".\n\nWould you like \'coffee\' or \'tea\'?");
 
 		System.out.println("\n+-----------------------+\n");
