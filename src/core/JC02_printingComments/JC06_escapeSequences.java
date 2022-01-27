74f78b022b05bdbbb69070e04eef7a34334f7958
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-01-27 09:49:22 +0300

    Türkçe yorum satırları eklendi

--- src/core/JC02_printingComments/JC06_escapeSequences.java
+++ src/core/JC02_printingComments/JC06_escapeSequences.java
@@ -10,7 +10,7 @@ public class JC06_escapeSequences {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		/**
+		/** ESCAPE SEQUENCES
 		 * \n = New Line -> Advances the cursor to the next line for subsequent
 		 * printing.
 		 * 
@@ -26,6 +26,23 @@ public class JC06_escapeSequences {
 		 * \' = Single quote -> Causes a single quotation mark to be printed.
 		 * 
 		 * \" = Double quote -> Causes a double quotation mark to be printed.
+		 *
+		 */
+
+		/** KAÇIŞ DİZELERİ
+		 * \n = Yeni Satır -> Sonraki yazdırma için imleci bir sonraki satıra ilerletir
+		 *
+		 * \t = Yatay Sekme -> İmlecin bir sonraki sekme durağına geçmesine neden olur. 1 Tab 4 boşluk içerir.
+		 *
+		 * \b = Backspace -> İmlecin bir konum geriye gitmesine veya sola hareket etmesine neden olur.
+		 *
+		 * \r = Return -> İmlecin bir sonraki satıra değil, geçerli satırın başına gitmesine neden olur.
+		 *
+		 * \\ = Backslash -> Bir ters eğik çizginin yazdırılmasına neden olur.
+		 *
+		 * \' = Single quote -> Tek bir tırnak işaretinin yazdırılmasına neden olur.
+		 *
+		 * \" = Double quote -> Çift tırnak işaretinin yazdırılmasına neden olur.
 		 *
 		 */
 
