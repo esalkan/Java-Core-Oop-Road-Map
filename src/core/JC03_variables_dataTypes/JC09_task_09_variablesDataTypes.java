14abfbcfd9d764b9a7595b432bac6bc2d1421421
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-01-27 10:08:19 +0300

    Türkçe yorum satırları eklendi

--- src/core/JC03_variables_dataTypes/JC09_task_09_variablesDataTypes.java
+++ src/core/JC03_variables_dataTypes/JC09_task_09_variablesDataTypes.java
@@ -9,14 +9,15 @@ public class JC09_task_09_variablesDataTypes {
 	/**
 	 * @param args
 	 */
-
-	// Store the following into variables: number of children, partner's name,
-	// geographic location, job title.
-
-	// Output your fortune to the screen like so: "You will be a X in Y,
-	// and married to Z with N kids"
-
 	public static void main(String[] args) {
+		/**
+		 * Store the following into variables: number of children, partner's name, geographic location, job title.
+		 * Output your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids"
+		 *
+		 * Aşağıdakileri değişkenlerde saklayın: çocuk sayısı, eşin adı, coğrafi konum, iş unvanı.
+		 * Kaderinizi ekrana şu şekilde yazdırın: "Y'de X olacaksın, ve Z ile evli ve N çocuklu"
+		 */
+
 		int noc = 3;
 		String name1 = "Kel Oğlan";
 		String name2 = "Ay Kız";
