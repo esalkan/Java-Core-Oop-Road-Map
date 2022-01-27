3db3b65228c8c27d464c5b2bb98747d8fbf3536b
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-01-27 10:06:18 +0300

    Türkçe yorum satırları eklendi

--- src/core/JC03_variables_dataTypes/JC08_task_08_variablesDataTypes.java
+++ src/core/JC03_variables_dataTypes/JC08_task_08_variablesDataTypes.java
@@ -15,6 +15,9 @@ public class JC08_task_08_variablesDataTypes {
 		 * Write a Java Program code for: Assign the value "IN 1491 COLUMBUS SAILED THE
 		 * OCEANBLUE" to an appropriate variable, write a program in Java to change the
 		 * year in the statement above from 1491 to 1492.
+		 *
+		 * Bir Java Program kodu yazın: "IN 1491 COLUMBUS SAILED THE OCEANBLUE" değerini uygun bir değişkene atayın,
+		 * yukarıdaki ifadede yılı 1491'den 1492'ye değiştirmek için Java'da bir program yazın.
 		 */
 		int date = 1492;
 		String text = "IN " + date + " COLUMBUS SAILED THE OCEANBLUE";
