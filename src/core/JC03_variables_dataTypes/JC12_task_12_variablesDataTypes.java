44b326bbcf46538baf1d838533de0500cb155638
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-01-27 10:16:59 +0300

    Türkçe yorum satırları eklendi

--- src/core/JC03_variables_dataTypes/JC12_task_12_variablesDataTypes.java
+++ src/core/JC03_variables_dataTypes/JC12_task_12_variablesDataTypes.java
@@ -9,21 +9,17 @@ public class JC12_task_12_variablesDataTypes {
 	/**
 	 * @param args
 	 */
-
-	/*
-	 * 
-	 * 1- Declare a variable whose name is your first and last names combined, in
-	 * camelCase
-	 * 
-	 * 2- Assign the variable your first and last names, as a string
-	 * 
-	 * 3- Print in the console, specifying the variable, not the string, as the
-	 * message
-	 * 
-	 */
-
 	public static void main(String[] args) {
-
+		/*
+		 * 1- Declare a variable whose name is your first and last names combined, in camelCase
+		 * 2- Assign the variable your first and last names, as a string
+		 * 3- Print in the console, specifying the variable, not the string, as the message
+		 *
+		 *
+		 * 1- CamelCase'de adınız ve soyadınız birleştirilmiş bir değişken tanımlayın
+		 * 2- Değişkene adınızı ve soyadınızı bir dize olarak atayın
+		 * 3- Mesaj olarak dizeyi değil değişkeni belirterek konsolda yazdırın
+		 */
 		String firstLastName;
 		firstLastName = "Eyüp Sabri ALKAN";
 
