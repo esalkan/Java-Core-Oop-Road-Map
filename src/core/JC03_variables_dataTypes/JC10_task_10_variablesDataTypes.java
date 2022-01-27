735126ba784d5a9e48ad95963dedaf32a67eac4a
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-01-27 10:09:23 +0300

    Türkçe yorum satırları eklendi

--- src/core/JC03_variables_dataTypes/JC10_task_10_variablesDataTypes.java
+++ src/core/JC03_variables_dataTypes/JC10_task_10_variablesDataTypes.java
@@ -9,14 +9,13 @@ public class JC10_task_10_variablesDataTypes {
 	/**
 	 * @param args
 	 */
-
-	// Which of the followings are legal declarations?
-
 	public static void main(String[] args) {
+		// Which of the followings are legal declarations?
+		// Aşağıdakilerden hangisi doğru bir tanımlamadır?
 
 		// boolean b1, b2;
 		// String s1= "1", s2;
-		// double d1,double d2;
+		// double d1, double d2;
 		// int i1; int i2;
 		// int i3; i4;
 	}
