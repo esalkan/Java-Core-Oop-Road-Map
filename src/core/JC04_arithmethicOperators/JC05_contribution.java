d6d1b6b9e1bd541f27ab032ba006e6b53ee167bb
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-01-27 10:42:24 +0300

    Türkçe yorum satırları eklendi

--- src/core/JC04_arithmethicOperators/JC05_contribution.java
+++ src/core/JC04_arithmethicOperators/JC05_contribution.java
@@ -11,12 +11,13 @@ public class JC05_contribution {
 	 */
 	public static void main(String[] args) {
 		/*
-		 * 
 		 * This program calculates the amount of pay that will be contributed to a
 		 * retirement plan if 5%, 8% or 10% of monthly pay is withheld
-		 * 
 		 * Variables to hold the monthly pay and the amount of the contribution
-		 * 
+		 *
+		 * Bu program, aylık ücretin %5, %8 veya %10'unun kesintiye uğraması durumunda emeklilik planına yapılacak
+		 * katkı payını hesaplar.
+		 *
 		 */
 		int paymentAmount;
 		double percentage5, percentage8, percentage10;
@@ -27,15 +28,19 @@ public class JC05_contribution {
 		percentage10 = paymentAmount * 0.10;
 
 		// Display Monthly Payment
+		// Aylık Ödemeyi yazdır
 		System.out.println("Monthly Pay : $" + paymentAmount);
 
 		// Calculate and display 5% contribution
+		// %5 katkıyı hesapla ve göster
 		System.out.println("Retirement plan 5% : " + percentage5 + " per month.");
 
 		// Calculate and display 8% contribution
+		// %8 katkıyı hesapla ve göster.
 		System.out.println("Retirement plan 8% : " + percentage8 + " per month.");
 
 		// Calculate and display 10% contribution
+		// %10 katkıyı hesapla ve göster.
 		System.out.println("Retirement plan 10% :" + percentage10 + " per month.");
 	}
 
