055ce29f2131cc6b8a69037858f92a25df84c75d
Eyüp Sabri ALKAN <esalkan@icloud.com>
2022-01-27 09:42:59 +0300

    Türkçe yorum satırları eklendi

--- src/core/JC02_printingComments/JC02_printVSprintln.java
+++ src/core/JC02_printingComments/JC02_printVSprintln.java
@@ -11,16 +11,20 @@ public class JC02_printVSprintln {
 	 */
 	public static void main(String[] args) {
 
-		// println() is writing everything in a line and end of it put \n (new line end
-		// of it)
+		// println() is writing everything in a line and end of it put \n (new line end of it)
+		// println() her şeyi tek satıra yazdırır ve en sonunda \n ekleyerek bir alt satıra geçilmesini sağlar
 		System.out.println("this is written with println() method");
 		// print() is writing everything in a line
+		// print() her şeyi tek satıra yazdırır ve o satırda kalınmasını sağlar.
 		System.out.print("this is written with print() method");
 		
 		// is giving us an empty new line like one <br> as in html :)
+		// println() içerisine bir veri göndermez isek, bir alt satıra geç mantığından <br> html'de olduğu gibi
+		// bir satır boşluk oluşturur bir nevi ENTER tuşu işlemi görür.
 		System.out.println();
 		
 		// if you switch place of the above codes you will see everything is written in one line
+		// yukarıdaki kodların yerini değiştirirseniz her şeyin tek satırda yazıldığını göreceksiniz.
 		System.out.print("this is written with print() method");
 		System.out.println("this is written with println() method");
 
