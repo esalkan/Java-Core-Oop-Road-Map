aec470d695acb710211ddae2a6d509b55dabcd47
Eyüp Sabri ALKAN <esalkan@icloud.com>
2024-01-25 19:28:19 +0300

    Türkçe yorum satırları eklendi. Turkish comment lines added.

--- src/core/JC02_printingComments/JC01_helloWorld.java
+++ src/core/JC02_printingComments/JC01_helloWorld.java
@@ -5,17 +5,19 @@ package core.JC02_printingComments;
  * @github https://github.com/esalkan/java-notes
  */
 public class JC01_helloWorld {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// Print first message with JAVA
-		// print() println() methods will print what we give in it ;)
-		System.out.println("Maraba Zalım Dunya! :)");
-		System.out.println("Hello Cruel World! :)");
-		// Java ile ilk mesajımızı yazdıralım
-		// print() ve println() methodları içerisine yolladığımız veriyi ekrana yazdırmamızı sağlar.
-	}
+		// JAVA ile ilk mesajımızı yazdıralım
 
+		// print() println() methods will print what we give in it ;)
+		// print() ve println() methodları içerisine yolladığımız veriyi ekrana yazdırmamızı sağlar.
+
+		System.out.println("Maraba Zalım Dunya! :)"); // Print with new line - Yeni satırda yazdırma
+		System.out.println("Hello Cruel World! :)"); // Print with new line - Yeni satırda yazdırma
+		System.out.println(); // Blank Line - Boş Satır
+		System.out.print("Hello World! :)"); // Print without new line - Yeni satır olmadan yazdırma
+	}
 }
